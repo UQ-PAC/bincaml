52835669d03c1a2062b6fd49d7d1bf972a62b6ef
am <a.michael1@uq.edu.au>
2025-12-12 16:15:25 +1000

    fixup

--- lib/transforms/livevars.ml
+++ lib/transforms/livevars.ml
@@ -5,35 +5,16 @@ open Types
 module V = Set.Make (Var)
 module B = Map.Make (Var)
 
-let show_v b =
-  let open Containers_pp in
-  let open Containers_pp.Infix in
-  let x =
-    fill
-      (text "," ^ newline)
-      (V.to_list b |> List.map (fun v -> Containers_pp.text (Var.name v)))
-  in
-  Pretty.to_string ~width:80 x
-
+(** live vars transfer function for a statement *)
 let tf_stmt_live init s =
   let assigns = V.diff init (Stmt.assigned V.empty s) in
   Stmt.free_vars assigns s
 
+(** live vars transfer function for a block *)
 let tf_block (init : V.t) (b : (Var.t, BasilExpr.t) Block.t) =
   Block.fold_backwards ~f:tf_stmt_live ~phi:(fun f _ -> f) ~init b
 
-module WTO = Graph.WeakTopological.Make (struct
-  open Procedure
-  open G
-
-  type t = G.t
-
-  module V = G.V
-
-  let iter_succ = G.iter_pred
-  let iter_vertex = G.iter_vertex
-end)
-
+(** Boundockle live-variable analysis using ocamlgraph's chaotic iteration *)
 module LV =
   Graph.ChaoticIteration.Make
     (Procedure.RevG)
@@ -74,6 +55,16 @@ let run (p : Program.proc) =
     Option.get_or ~default:V.empty (Option.flat_map (LV.M.find_opt v) res)
 
 let label (r : Procedure.G.vertex -> V.t) (v : Procedure.G.vertex) =
+  let show_v b =
+    let open Containers_pp in
+    let open Containers_pp.Infix in
+    let x =
+      fill
+        (text "," ^ newline)
+        (V.to_list b |> List.map (fun v -> Containers_pp.text (Var.name v)))
+    in
+    Pretty.to_string ~width:80 x
+  in
   show_v (r v)
 
 let print_g res = Viscfg.dot_labels (fun v -> Some (label res v))
@@ -107,129 +98,10 @@ let%expect_test _ =
     forall(v1:bv1 :: eq(v2:bv1, forall(v2:bv1 :: booland(v1:bv1, v2:bv1, v3:bv1))))
     forall(v1:bv1 :: eq(0x16:bv5, forall(v2:bv1 :: booland(v1:bv1, v2:bv1, 0x16:bv5)))) |}]
 
-module Interproc = struct
-  type value = { proc_summary : V.t ID.Map.t; lives : V.t }
-
-  (** fairly inefficient inter-procedural live-variables analysis; takes 7s on
-      cntlm early IR.
-
-      Solves over the call-graph and re-solves each procedure
-      (intra-procedurally) every time a dependency is changed.
-
-      Passes the summary through the abstract domain of the intra-procedural
-      solve so that the transfer function can inspect it to handle calls. This
-      is pretty janky, I would like a better solution. *)
-
-  let free_vars_stmt summary (init : V.t)
-      (s : (Var.t, Var.t, BasilExpr.t) Stmt.t) : V.t =
-    let f_expr a v =
-      match v with
-      | `Expr v -> V.union (BasilExpr.free_vars v) a
-      | `Var v -> V.add v a
-    in
-    let filter_glob =
-      V.filter (function (v : Var.t) ->
-          Var.equal_declaration_scope (Var.scope v) Global)
-    in
-    let free = Stmt.iter_rexpr s |> Iter.fold f_expr init |> filter_glob in
-    let open Iter.Infix in
-    match s with
-    | Stmt.(Instr_Call { procid }) -> (
-        ID.Map.get procid summary |> function
-        | None -> free
-        | Some es ->
-            let globs = filter_glob es in
-            V.union free globs)
-    | _ -> free
-
-  let tf_block summary (init : V.t) (b : (Var.t, BasilExpr.t) Block.t) =
-    let tf_stmt init s =
-      let assigns = V.diff init (Stmt.assigned V.empty s) in
-      free_vars_stmt summary assigns s
-    in
-    Block.fold_backwards ~f:tf_stmt ~phi:(fun f _ -> f) ~init b
-
-  let tf_block (st : value) b =
-    let lives = tf_block st.proc_summary st.lives b in
-    { st with lives }
-
-  let join a b = V.union a.lives b.lives
-
-  module ILV =
-    Graph.Fixpoint.Make
-      (Procedure.G)
-      (struct
-        open Procedure
-
-        type vertex = G.E.vertex
-        type edge = G.E.t
-        type g = G.t
-        type data = value
-
-        let direction = Graph.Fixpoint.Backward
-        let equal a b = V.equal a.lives b.lives
-
-        let join (a : data) (b : data) =
-          { a with lives = V.union a.lives b.lives }
-
-        let analyze (e : edge) d : data =
-          match G.E.label e with Block b -> tf_block d b | _ -> d
-      end)
-
-  let tf_proc (prog : Program.t) proc_summary (pid : ID.t) =
-    Trace.with_span ~__FILE__ ~__LINE__ "liveness-solve-proc" @@ fun _ ->
-    let proc = ID.Map.find pid prog.procs in
-    let analyse graph =
-      let res =
-        ILV.analyze (function e -> { proc_summary; lives = V.empty }) graph
-      in
-      let entry = res Procedure.Vert.Entry in
-      let n = ID.Map.add pid entry.lives proc_summary in
-      n
-    in
-    Procedure.graph proc |> Option.map analyse
-    |> Option.get_or ~default:proc_summary
-
-  module CG = Program.CallGraph.G
-
-  module type InterLVAnalysis = sig
-    val analyze :
-      (CG.vertex -> V.t ID.Map.t) -> CG.t -> CG.vertex -> V.t ID.Map.t
-  end
-
-  module InterprocLV (Prog : sig
-    val prog : Program.t
-  end) =
-    Graph.Fixpoint.Make
-      (CG)
-      (struct
-        type vertex = CG.E.vertex
-        type edge = CG.E.t
-        type g = CG.t
-        type data = V.t ID.Map.t [@@deriving eq]
-
-        let direction = Graph.Fixpoint.Backward
-        let equal = equal_data
-
-        let join (a : data) (b : data) : data =
-          ID.Map.union (fun id a b -> Some (V.union a b)) a b
-
-        let analyze (e : edge) d : data =
-          match CG.E.label e with Proc id -> tf_proc Prog.prog d id | _ -> d
-      end)
-
-  let analyse_prog p =
-    let open Program in
-    let default = ID.Map.map (fun _ -> V.empty) p.procs in
-    let callgraph = Program.CallGraph.make_call_graph p in
-    let module M : InterLVAnalysis = InterprocLV (struct
-      let prog = p
-    end) in
-    Trace.with_span ~__FILE__ ~__LINE__ "liveness-solve-callgraph" @@ fun _ ->
-    M.analyze (fun v -> default) callgraph
-end
-
 module DSE = struct
+  (** Dead-store elimination for local variables based on intraprocedural live
+      variables analysis *)
+
   let filter_dead (live : V.t) (b : (Var.t, BasilExpr.t) Block.t) =
     let r =
       Block.fold_backwards
@@ -254,7 +126,6 @@ module DSE = struct
 
   let sane_transform (p : Program.proc) =
     let live = run p in
-    CCIO.with_out "live.dot" (fun c -> print_live_vars_dot (Format.of_chan c) p);
     let blocks = Procedure.blocks_to_list p in
     List.fold_left
       (fun p b ->
