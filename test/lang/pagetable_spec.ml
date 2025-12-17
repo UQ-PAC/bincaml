3a09b47184e096a331c5595ddb82ce74144c0b72
rina <k@rina.fyi>
2025-12-17 18:37:19 +1000

    blahhhhhh. does it work now??????

--- test/lang/pagetable_spec.ml
+++ test/lang/pagetable_spec.ml
@@ -34,18 +34,72 @@ module Model = struct
              (addr, state)
         |> snd
 
-  let arb_cmd = failwith ""
+  let read_byte ~addr st =
+    IntegerMap.find_opt addr st |> Option.value ~default:'\x00'
+
+  (** Generates a non-negative {!Z.t} (currently up to int64 size). *)
+  let arb_posint =
+    let open QCheck.Gen in
+    let+ n = int64 in
+    Z.of_int64_unsigned n
+
+  let gen_addr st =
+    let open QCheck.Gen in
+    let* addr =
+      match IntegerMap.bindings st |> List.map fst with
+      | [] -> arb_posint
+      | addrs -> oneof [ oneofl addrs; arb_posint ]
+    in
+    let+ fuzz = small_signed_int in
+    Z.(addr + ~$fuzz)
+
+  let arb_cmd st =
+    let open QCheck.Gen in
+    QCheck.make ~print:show_cmd
+    @@
+    let* nbytes = small_nat in
+    let* addr = gen_addr st in
+    oneof
+      [
+        return (Read { addr; nbytes });
+        (let+ v = arb_posint in
+         Write { addr; bv = Bitvec.create ~size:(nbytes * 8) v });
+      ]
 end
 
 module Spec : STM.Spec = struct
   include Model
 
-  type sut = Lang.Interp.PageTable.t
+  open struct
+    module PageTable = Lang.Interp.PageTable
+  end
 
-  let init_sut () = Lang.Interp.PageTable.create ~page_len:9 ?use_random_init:None ()
+  type sut = PageTable.t
+
+  let init_sut () = PageTable.create ~page_len:9 ?use_random_init:None ()
   let cleanup _ = ()
 
-  let precond cmd st = true
-  let run cmd st = failwith ""
-  let postcond cmd st res = true
+  let precond cmd _ =
+    match cmd with Read _ -> true | Write { bv; _ } -> bv.w mod 8 == 0
+
+  type 'a STM.ty += BitvecTy : Bitvec.t STM.ty
+
+  let bitvec_show = (BitvecTy, Bitvec.to_string)
+
+  let run cmd sut =
+    match cmd with
+    | Read { addr; nbytes } ->
+        STM.Res (bitvec_show, PageTable.read_bv ~addr ~nbits:(nbytes * 8) sut)
+    | Write { addr; bv } -> STM.Res (STM.unit, PageTable.write_bv sut ~addr bv)
+
+  let postcond cmd oldst res =
+    match (cmd, res) with
+    | Read { addr; nbytes }, STM.Res ((BitvecTy, _), bv) ->
+        bv.w = nbytes * 8
+        && Bitvec.to_bytes bv |> Bytes.to_seq
+           |> Seq.fold_lefti
+                (fun ok i c -> ok && read_byte ~addr:Z.(addr + ~$i) oldst = c)
+                true
+    | Write _, STM.Res ((STM.Unit, _), ()) -> true
+    | _ -> false
 end
