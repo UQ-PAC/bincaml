1361543e1e1cd3daa256d701ecf3865186d4b164
bpaul <bpaul@bpaul.xyz>
2026-01-22 16:18:54 +1000

    fmt

--- lib/analysis/ide_live.ml
+++ lib/analysis/ide_live.ml
@@ -90,11 +90,16 @@ module IDELive = struct
     | Label _ -> Iter.empty
 
   let transfer_stub (s : stub_info) d =
-      match d with
-      | Lambda -> Iter.singleton (d, IdEdge)
-      |> Iter.append (Iter.of_list s.formal_in |> Iter.map (fun v -> (Label v, ConstEdge live)))
-      |> Iter.append (Iter.of_list s.globals |> Iter.map (fun v -> (Label v, ConstEdge live)))
-      | _ -> Iter.empty
+    match d with
+    | Lambda ->
+        Iter.singleton (d, IdEdge)
+        |> Iter.append
+             (Iter.of_list s.formal_in
+             |> Iter.map (fun v -> (Label v, ConstEdge live)))
+        |> Iter.append
+             (Iter.of_list s.globals
+             |> Iter.map (fun v -> (Label v, ConstEdge live)))
+    | _ -> Iter.empty
 
   let transfer stmt d =
     let open Stmt in
