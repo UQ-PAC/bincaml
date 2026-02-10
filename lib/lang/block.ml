0313ea96fce2bbe16a4d7e9176d4ee789a81bb8b
rina <k@rina.fyi>
2026-02-10 17:10:58 +1000

    use dominator order in procedure printing
    it's much faster

--- lib/lang/block.ml
+++ lib/lang/block.ml
@@ -35,6 +35,7 @@ let pretty_phi show_lvar show_var v =
   lhs ^ text " := phi" ^ (bracket "(" (fill (text "," ^ newline) rhs)) ")"
 
 let pretty show_lvar show_var show_expr ?(terminator = []) ?block_id b =
+  Trace_core.with_span ~__FILE__ ~__LINE__ "pretty-block" @@ fun _ ->
   let open Containers_pp in
   let open Containers_pp.Infix in
   let phi =
