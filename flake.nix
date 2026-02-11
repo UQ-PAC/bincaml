ba9debd433e7d2308c62b47c78deb65a1d5ae762
McArthur-Alford <mcarthur.alford@proton.me>
2026-02-11 16:00:47 +1000

    Nix flake for packaging bincaml and devshell for testing

---
+++ flake.nix
@@ -0,0 +1,92 @@
+{
+  inputs = {
+    opam-nix.url = "github:tweag/opam-nix";
+    opam-nix.inputs.opam-repository.follows = "opam-repository";
+    opam-repository = {
+      url = "github:ocaml/opam-repository";
+      flake = false;
+    };
+    pac-opam = {
+      url = "github:uq-pac/opam-repository";
+      flake = false;
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.follows = "opam-nix/nixpkgs";
+  };
+  outputs =
+    {
+      flake-utils,
+      opam-nix,
+      nixpkgs,
+      pac-opam,
+      opam-repository,
+      ...
+    }:
+    let
+      package = "bincaml";
+    in
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        on = opam-nix.lib.${system};
+        devPackagesQuery = {
+          menhir = "*";
+          zarith = "*";
+          fix = "*";
+          trace = "*";
+          trace-tef = "*";
+          containers = "*";
+          iter = "*";
+          containers-data = "*";
+          ppx_deriving = "*";
+          ocamlgraph = "*";
+          intPQueue = "*";
+          cmdliner = "*";
+          pp_loc = "*";
+          fmt = "*";
+          patricia-tree = "*";
+          odig = "*";
+          sherlodoc = "*";
+          ppx_expect_nobase = "*";
+          alcotest = "*";
+          qcheck-core = "*";
+          qcheck-alcotest = "*";
+          qcheck-stm = "*";
+          ocaml-lsp-server = "*";
+          ocamlformat = "*";
+        };
+        query = devPackagesQuery // {
+          ocaml-compiler = "5.3.0";
+          ocaml-variants = "5.3.0+options";
+          ocaml-option-fp = "*";
+          ocaml-option-flambda = "*";
+          basil_lsp = "*";
+        };
+        scope = on.buildOpamProject' {
+          repos = [
+            pac-opam
+            opam-repository
+          ];
+        } ./. query;
+        overlay = final: prev: {
+          ${package} = prev.${package}.overrideAttrs (_: {
+            doNixSupport = false;
+          });
+        };
+        scope' = scope.overrideScope overlay;
+        main = scope'.${package};
+        devPackages = builtins.attrValues (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope');
+      in
+      {
+        legacyPackages = scope';
+
+        packages.default = main;
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ main ];
+          buildInputs = devPackages ++ [ ];
+        };
+      }
+    );
+}
