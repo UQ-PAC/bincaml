593ac823b9d9f13d5eed9731441c16b9deb6fb0d
JTrenerry <105094182+JTrenerry@users.noreply.github.com>
2026-02-13 10:52:19 +1000

    modules everywhere

--- lib/transforms/asd.ml
+++ lib/transforms/asd.ml
@@ -1,137 +1,147 @@
 open Bincaml_util.Common
 
-(* |add_unique| -- adds e to list l only if it is not already in l *)
-let add_unique e l = if List.mem e l then l else e :: l
+module Polarity = struct
+  type t = Pos | Neg [@@deriving ord, eq, show]
 
-(* |list_union| -- returns union of the two input lists *)
-let rec list_union l1 = function
-  | [] -> l1
-  | x :: xs -> list_union (add_unique x l1) xs
+  let not p = match p with Pos -> Neg | Neg -> Pos
+  let positive = equal Pos
+end
 
-(* |index| -- finds Some index of x in xs, otherwise returns None *)
-let index x xs =
-  let rec aux c = function
-    | [] -> None
-    | y :: ys -> if x = y then Some c else aux (c + 1) ys
-  in
-  aux 0 xs
+module CType = struct
+  type t = C_Int | C_BV of int | C_Float | C_Bool [@@deriving ord, eq]
 
-type c_type = C_Int | C_BV of int | C_Float | C_Bool [@@deriving ord, eq]
+  let show = function
+    | C_Int -> "int"
+    | C_BV size -> "bv" ^ string_of_int size
+    | C_Float -> "float"
+    | C_Bool -> "bool"
+end
 
-let show_c_type = function
-  | C_Int -> "int"
-  | C_BV size -> "bv" ^ string_of_int size
-  | C_Float -> "float"
-  | C_Bool -> "bool"
+module InferredType = struct
+  type t =
+    | Top
+    | Bottom
+    | Paren of t
+    | Union of t * t (* type ∪ type *)
+    | Sect of t * t (* type ∩ type *)
+    | Pointer of t * t (* ptr(lb, ub) *)
+    | Function of
+        string
+        * t StringMap.t
+        * t StringMap.t (* list of inputs and list of outputs *)
+    | Field of field
+    | Record of field list (* A list of fields in the record *)
+    | TypeVar of string
+    | Recursive of t * t
+    | Atom of CType.t
 
-type ty =
-  | Top
-  | Bottom
-  | Paren of ty
-  | Union of ty * ty (* type ∪ type *)
-  | Sect of ty * ty (* type ∩ type *)
-  | Pointer of ty * ty (* ptr(lb, ub) *)
-  | Function of
-      string
-      * ty StringMap.t
-      * ty StringMap.t (* list of inputs and list of outputs *)
-  | Field of field
-  | Record of field list (* A list of fields in the record *)
-  | TypeVar of string
-  | Recursive of ty * ty
-  | Atom of c_type
+  and field = { offset : int; size : int; ty : t }
 
-and field = { offset : int; size : int; ty : ty }
+  let rec show = function
+    | Top -> "⊤"
+    | Bottom -> "⊥"
+    | Atom c -> CType.show c
+    | TypeVar id -> Printf.sprintf "TypeVar %s" id
+    | Recursive (t1, t2) -> Printf.sprintf "μ%s.%s" (show t1) (show t2)
+    | Paren ty -> Printf.sprintf "(%s)" @@ show ty
+    | Union (t1, t2) -> Printf.sprintf "%s ⊔ %s" (show t1) (show t2)
+    | Sect (t1, t2) -> Printf.sprintf "%s ⊓ %s" (show t1) (show t2)
+    | Pointer (lb, ub) -> Printf.sprintf "ptr(%s, %s)" (show lb) (show ub)
+    | Function (name, ins, outs) ->
+        Printf.sprintf "(%s) → (%s)"
+          (Iter.to_string show (StringMap.values ins))
+          (Iter.to_string show (StringMap.values outs))
+    | Field field -> show_field field
+    | Record fields ->
+        Printf.sprintf "{ %s }" @@ List.to_string show_field fields
 
-let rec show_ty = function
-  | Top -> "⊤"
-  | Bottom -> "⊥"
-  | Atom c -> show_c_type c
-  | TypeVar id -> Printf.sprintf "TypeVar %s" id
-  | Recursive (t1, t2) -> Printf.sprintf "μ%s.%s" (show_ty t1) (show_ty t2)
-  | Paren ty -> Printf.sprintf "(%s)" @@ show_ty ty
-  | Union (t1, t2) -> Printf.sprintf "%s ⊔ %s" (show_ty t1) (show_ty t2)
-  | Sect (t1, t2) -> Printf.sprintf "%s ⊓ %s" (show_ty t1) (show_ty t2)
-  | Pointer (lb, ub) -> Printf.sprintf "ptr(%s, %s)" (show_ty lb) (show_ty ub)
-  | Function (name, ins, outs) ->
-      Printf.sprintf "(%s) → (%s)"
-        (Iter.to_string show_ty (StringMap.values ins))
-        (Iter.to_string show_ty (StringMap.values outs))
-  | Field field -> show_field field
-  | Record fields -> Printf.sprintf "{ %s }" @@ List.to_string show_field fields
+  and show_field { offset; size; ty } =
+    Printf.sprintf "(%d, %d): %s" offset size (show ty)
 
-and show_field { offset; size; ty } =
-  Printf.sprintf "(%d, %d): %s" offset size (show_ty ty)
+  let rec compare type1 type2 =
+    match (type1, type2) with
+    | Top, Top | Bottom, Bottom -> 0
+    | Atom a, Atom b -> CType.compare a b
+    | TypeVar a, TypeVar b -> String.compare a b
+    | Recursive (a, b), Recursive (c, d) ->
+        let c = compare a c in
+        if c <> 0 then c else compare b d
+    | Paren a, Paren b -> compare a b
+    | Union (a, b), Union (a2, b2) ->
+        let c = compare a a2 in
+        if c <> 0 then c else compare b b2
+    | Sect (a, b), Sect (a2, b2) ->
+        let c = compare a a2 in
+        if c <> 0 then c else compare b b2
+    | Pointer (a, b), Pointer (a2, b2) ->
+        let c = compare a a2 in
+        if c <> 0 then c else compare b b2
+    | Function (name, ins, outs), Function (name2, ins2, outs2) ->
+        let c = String.compare name name2 in
+        if c <> 0 then c
+        else
+          let c = StringMap.compare compare ins ins2 in
+          if c <> 0 then c else StringMap.compare compare outs outs2
+    | ( Field { offset; size; ty },
+        Field { offset = offset2; size = size2; ty = ty2 } ) ->
+        let c = Int.compare offset offset2 in
+        if c <> 0 then c
+        else
+          let c = Int.compare size size2 in
+          if c <> 0 then c else compare ty ty2
+    | Record fields, Record fields2 ->
+        List.compare (fun { ty } { ty = ty2 } -> compare ty ty2) fields fields2
+    | _ -> 1
 
-let rec compare_ty type1 type2 =
-  match (type1, type2) with
-  | Top, Top | Bottom, Bottom -> 0
-  | Atom a, Atom b -> compare_c_type a b
-  | TypeVar a, TypeVar b -> String.compare a b
-  | Recursive (a, b), Recursive (c, d) ->
-      let c = compare_ty a c in
-      if c <> 0 then c else compare_ty b d
-  | Paren a, Paren b -> compare_ty a b
-  | Union (a, b), Union (a2, b2) ->
-      let c = compare_ty a a2 in
-      if c <> 0 then c else compare_ty b b2
-  | Sect (a, b), Sect (a2, b2) ->
-      let c = compare_ty a a2 in
-      if c <> 0 then c else compare_ty b b2
-  | Pointer (a, b), Pointer (a2, b2) ->
-      let c = compare_ty a a2 in
-      if c <> 0 then c else compare_ty b b2
-  | Function (name, ins, outs), Function (name2, ins2, outs2) ->
-      let c = String.compare name name2 in
-      if c <> 0 then c
-      else
-        let c = StringMap.compare compare_ty ins ins2 in
-        if c <> 0 then c else StringMap.compare compare_ty outs outs2
-  | ( Field { offset; size; ty },
-      Field { offset = offset2; size = size2; ty = ty2 } ) ->
-      let c = compare offset offset2 in
-      if c <> 0 then c
-      else
-        let c = compare size size2 in
-        if c <> 0 then c else compare_ty ty ty2
-  | Record fields, Record fields2 ->
-      List.compare (fun { ty } { ty = ty2 } -> compare_ty ty ty2) fields fields2
-  | _ -> 1
+  let equal a b = Stdlib.(==) 0 @@ compare a b
+end
 
-(* left hand side maps to left hand side ty <= ty *)
-module TySet = Set.Make (struct
-  type t = ty
+module TySet = struct
+  module S = Set.Make (struct
+    type t = InferredType.t
 
-  let compare = compare_ty
-end)
+    let compare = InferredType.compare
+  end)
 
-type type_constraint = { lb : TySet.t; ub : TySet.t }
-type constraint_state = type_constraint StringMap.t
+  include S
 
-let constraint_state_equals { lb; ub } { lb = lb2; ub = ub2 } =
-  if TySet.equal lb lb2 then if TySet.equal ub ub2 then true else false
-  else false
+  let show ts = to_list ts |> List.map InferredType.show |> String.concat ", "
+end
 
-let show_ty_set ts = TySet.to_list ts |> List.map show_ty |> String.concat ", "
+module ConstraintState = struct
+  module TypeConstraint = struct
+    type t = { lb : TySet.t; ub : TySet.t }
 
-let show_constraint_state (m : type_constraint StringMap.t) : string =
-  StringMap.bindings m
-  |> List.map (fun (name, { lb; ub }) ->
-      Printf.sprintf "%s: lower [%s], upper [%s]" name (show_ty_set lb)
-        (show_ty_set ub))
-  |> String.concat "\n"
+    let equal { lb; ub } { lb = lb2; ub = ub2 } =
+      TySet.equal lb lb2 && TySet.equal ub ub2
+  end
 
-(* Helpers to actually add something to the upper / lower bounds *)
-let add_ub st name ty =
-  StringMap.update name
-    (function
-      | None -> Some { lb = TySet.empty; ub = TySet.singleton ty }
-      | Some c -> Some { c with ub = TySet.add ty c.ub })
-    st
+  type t = TypeConstraint.t StringMap.t
 
-let add_lb st name ty =
-  StringMap.update name
-    (function
-      | None -> Some { ub = TySet.empty; lb = TySet.singleton ty }
-      | Some c -> Some { c with lb = TySet.add ty c.lb })
-    st
+  let equal = StringMap.equal TypeConstraint.equal
+
+  let show m =
+    StringMap.bindings m
+    |> List.map (fun (name, ({ lb; ub } : TypeConstraint.t)) ->
+        Printf.sprintf "%s: lower [%s], upper [%s]" name (TySet.show lb)
+          (TySet.show ub))
+    |> String.concat "\n"
+
+  let add_ub st name ty =
+    StringMap.update name
+      (function
+        | None ->
+            Some
+              ({ lb = TySet.empty; ub = TySet.singleton ty } : TypeConstraint.t)
+        | Some c -> Some { c with ub = TySet.add ty c.ub })
+      st
+
+  let add_lb st name ty =
+    StringMap.update name
+      (function
+        | None ->
+            Some
+              ({ ub = TySet.empty; lb = TySet.singleton ty } : TypeConstraint.t)
+        | Some c -> Some { c with lb = TySet.add ty c.lb })
+      st
+end
--- lib/transforms/type_automata.ml
+++ lib/transforms/type_automata.ml
@@ -1,237 +1,248 @@
 open Bincaml_util.Common
 open Asd
 
-type sigma =
-  | Ep
-  | StoreLabel
-  | LoadLabel
-  | Reclabel of int * int
-  | FnIn of string
-  | FnOut of string
+module Sigma = struct
+  type t =
+    | Ep
+    | StoreLabel
+    | LoadLabel
+    | Reclabel of int * int
+    | FnIn of string
+    | FnOut of string
 
-let show_sigma (sigma : sigma) =
-  match sigma with
-  | Ep -> "ε"
-  | StoreLabel -> "Store Label"
-  | LoadLabel -> "Load Label"
-  | Reclabel (n, m) -> Printf.sprintf "Record Label %d %d" n m
-  | FnIn n -> Printf.sprintf "Function in %s" n
-  | FnOut n -> Printf.sprintf "Function out %s" n
+  let show = function
+    | Ep -> "ε"
+    | StoreLabel -> "Store Label"
+    | LoadLabel -> "Load Label"
+    | Reclabel (n, m) -> Printf.sprintf "Record Label %d %d" n m
+    | FnIn n -> Printf.sprintf "Function in %s" n
+    | FnOut n -> Printf.sprintf "Function out %s" n
 
-let equal_sigma s1 s2 =
-  match (s1, s2) with
-  | Ep, Ep | StoreLabel, StoreLabel | LoadLabel, LoadLabel -> true
-  | Reclabel (n, m), Reclabel (n1, m1) -> n = n1 && m = m1
-  | FnIn n, FnIn n1 | FnOut n, FnOut n1 -> String.equal n n1
-  | _ -> false
+  let equal a b =
+    match (a, b) with
+    | Ep, Ep | StoreLabel, StoreLabel | LoadLabel, LoadLabel -> true
+    | Reclabel (n, m), Reclabel (n1, m1) -> n = n1 && m = m1
+    | FnIn n, FnIn n1 | FnOut n, FnOut n1 -> String.equal n n1
+    | _ -> false
 
-type 's automata = {
-  mutable states : 's list;
-  transitions : ('s, (sigma, 's) Hashtbl.t) Hashtbl.t;
-  start : 's;
-  name : string;
-}
+  let is_epislon = equal Ep
+end
 
-let set_states m qs = m.states <- qs
-let equal_state (p1, ty1) (p2, ty2) = compare_ty ty1 ty2 = 0 && Bool.equal p1 p2
+module State = struct
+  type t = Polarity.t * InferredType.t
 
-let get_transitions m =
-  Hashtbl.fold
-    (fun s ats acc -> Hashtbl.fold (fun a t acc' -> (s, a, t) :: acc') ats acc)
-    m.transitions []
+  let equal (p1, ty1) (p2, ty2) =
+    InferredType.equal ty1 ty2 && Polarity.equal p1 p2
+end
 
-let get_next_states m s =
-  match Hashtbl.find_opt m.transitions s with
-  | None -> Iter.empty
-  | Some states -> Hashtbl.values states
+module TypeAutomata = struct
+  type t = {
+    mutable states : State.t list;
+    transitions : (State.t, (Sigma.t, State.t) Hashtbl.t) Hashtbl.t;
+    start : State.t;
+    name : string;
+  }
 
-let get_prev_states m t =
-  Hashtbl.fold
-    (fun s v acc ->
-      Hashtbl.fold
-        (fun a' t' acc' -> if equal_state t t' then s :: acc' else acc')
-        v acc)
-    m.transitions []
+  let set_states m qs = m.states <- qs
 
-let filter_states_inplace m f =
-  set_states m (List.filter f m.states);
-  Hashtbl.filter_map_inplace
-    (fun s ts -> if f s then Some ts else None)
-    m.transitions
+  let get_transitions m =
+    Hashtbl.fold
+      (fun s ats acc ->
+        Hashtbl.fold (fun a t acc' -> (s, a, t) :: acc') ats acc)
+      m.transitions []
 
-let merge_states_inplace m ((p1, ty1) as p) ((p2, ty2) as q) =
-  filter_states_inplace m (fun (p3, ty3) ->
-      Bool.equal p3 p2 && compare_ty ty2 ty3 = 0);
-  Hashtbl.iter
-    (fun _ v ->
-      Hashtbl.filter_map_inplace
-        (fun _ t -> Some (if Bool.equal p1 p2 then p else t))
-        v)
-    m.transitions
+  let get_next_states m s =
+    match Hashtbl.find_opt m.transitions s with
+    | None -> Iter.empty
+    | Some states -> Hashtbl.values states
 
-let rec list_union l1 = function
-  | [] -> l1
-  | x :: xs -> list_union (add_unique x l1) xs
+  let get_prev_states m t =
+    Hashtbl.fold
+      (fun s v acc ->
+        Hashtbl.fold
+          (fun a' t' acc' -> if State.equal t t' then s :: acc' else acc')
+          v acc)
+      m.transitions []
 
-let remove_ep m =
-  (*
-    NOTE:
-      If there is an epilson edge like below
+  let filter_states_inplace m f =
+    set_states m (List.filter f m.states);
+    Hashtbl.filter_map_inplace
+      (fun s ts -> if f s then Some ts else None)
+      m.transitions
 
-      start -- ep --> bad boy -- a --> end
+  let merge_states_inplace m ((p1, ty1) as s1) ((p2, ty2) as s2) =
+    filter_states_inplace m (fun s3 -> State.equal s2 s3);
+    Hashtbl.iter
+      (fun _ v ->
+        Hashtbl.filter_map_inplace
+          (fun _ t -> Some (if Polarity.equal p1 p2 then s1 else t))
+          v)
+      m.transitions
 
-      then start must raise the bad boy's children i.e.
-
-      start -- a --> end
-
-      If there are no kids, just kill
-
-      start -- ep --> bad boy
-
-      start
-
-      What about a bigger case?
-
-      start -- ep --> middle -- ep --> middle2 -- a --> end
-
-      start -- a --> end
-
-      works
-  *)
-  let removal =
-    let rec helper acc current_state =
-      (* Do the lower states first, I am scared about what happens if loop *)
-      let acc = Iter.fold helper acc @@ get_next_states m current_state in
-      let acc =
+  let remove_ep m =
+    (*
+      NOTE:
+        If there is an epilson edge like below
+  
+        start -- ep --> bad boy -- a --> end
+  
+        then start must raise the bad boy's children i.e.
+  
+        start -- a --> end
+  
+        If there are no kids, just kill
+  
+        start -- ep --> bad boy
+  
+        start
+  
+        What about a bigger case?
+  
+        start -- ep --> middle -- ep --> middle2 -- a --> end
+  
+        start -- a --> end
+  
+        works
+    *)
+    let removal =
+      let rec helper acc current_state =
+        (* Do the lower states first, I am scared about what happens if loop *)
+        let acc = Iter.fold helper acc @@ get_next_states m current_state in
+        let acc =
+          match Hashtbl.find_opt m.transitions current_state with
+          | None -> acc
+          | Some edges ->
+              Hashtbl.fold
+                (fun edge end_state acc ->
+                  if not @@ Sigma.is_epislon edge then acc
+                  (* There exists a epislon edge from start -> end *)
+                    else
+                    let orphans = Hashtbl.find_all m.transitions end_state in
+                    Hashtbl.remove m.transitions end_state;
+                    List.iter (Hashtbl.iter (Hashtbl.add edges)) orphans;
+                    end_state :: acc)
+                edges acc
+        in
         match Hashtbl.find_opt m.transitions current_state with
         | None -> acc
         | Some edges ->
-            Hashtbl.fold
-              (fun edge end_state acc ->
-                if not @@ equal_sigma edge Ep then acc
-                (* There exists a epislon edge from start -> end *)
-                  else
-                  let orphans = Hashtbl.find_all m.transitions end_state in
-                  Hashtbl.remove m.transitions end_state;
-                  List.iter (Hashtbl.iter (Hashtbl.add edges)) orphans;
-                  end_state :: acc)
-              edges acc
+            let eps = Hashtbl.find_all edges Ep in
+            for i = 0 to List.length eps do
+              Hashtbl.remove (Hashtbl.find m.transitions current_state) Ep
+            done;
+            acc
       in
-      match Hashtbl.find_opt m.transitions current_state with
-      | None -> acc
-      | Some edges ->
-          let eps = Hashtbl.find_all edges Ep in
-          for i = 0 to List.length eps do
-            Hashtbl.remove (Hashtbl.find m.transitions current_state) Ep
-          done;
-          acc
+      helper [] m.start
     in
-    helper [] m.start
-  in
-  filter_states_inplace m (fun state ->
-      not
-      @@ List.mem
-           ~eq:(fun (p1, ty1) (p2, ty2) ->
-             compare_ty ty1 ty2 = 0 && Bool.equal p1 p2)
-           state removal)
+    filter_states_inplace m (fun state ->
+        not
+        @@ List.mem
+             ~eq:(fun (p1, ty1) (p2, ty2) ->
+               InferredType.equal ty1 ty2 && Polarity.equal p1 p2)
+             state removal)
 
-let join (ty0 : ty) (ty1 : ty) : ty =
-  match (ty0, ty1) with
-  | Record fields0, Record fields1 ->
-      (* I think this could be improved, cause this is gross *)
-      let module FieldMap = Map.Make (struct
-        type t = int * int
+  let join (ty0 : InferredType.t) (ty1 : InferredType.t) : InferredType.t =
+    match (ty0, ty1) with
+    | Record fields0, Record fields1 ->
+        (* I think this could be improved, cause this is gross *)
+        let module FieldMap = Map.Make (struct
+          type t = int * int
 
-        let compare = Stdlib.compare
-      end) in
-      let fieldmap_to_field_list (map : ty FieldMap.t) =
-        FieldMap.bindings map
-        |> List.map (fun ((offset, size), ty) -> { offset; size; ty })
-      in
-      let fieldmap_of_list (fields : field list) : ty FieldMap.t =
-        List.fold_left
-          (fun acc { offset; size; ty } -> FieldMap.add (offset, size) ty acc)
-          FieldMap.empty fields
-      in
-      let f0 = fieldmap_of_list fields0 in
-      let f1 = fieldmap_of_list fields1 in
-      let joined_map =
-        FieldMap.merge_safe
-          ~f:(fun (offset, size) v ->
-            match v with
-            | `Both (a, b) -> Some (Union (a, b))
-            | `Left a | `Right a -> Some a)
-          f0 f1
-      in
-      Record (fieldmap_to_field_list joined_map)
-  | Pointer (a, b), Pointer (c, d) ->
-      (* ptr((a u c) n (b n d), (b n d)) *)
-      Pointer (Sect (Union (a, c), Sect (b, d)), Sect (b, d))
-  (* WARN: this is not how BinSub did it, but I think I am just smarter and had better DS *)
-  | Function (name0, ins0, outs0), Function (name1, ins1, outs1) ->
-      if not @@ String.equal name0 name1 then failwith "BOOOOM"
-      else
-        (* args are the same just just union over the args *)
-        let ins =
-          StringMap.merge_safe
-            ~f:(fun _ b ->
-              match b with
-              | `Both (l, r) -> Some (Sect (l, r))
-              | _ -> failwith "BOOOMM")
-            ins0 ins1
+          let compare = Stdlib.compare
+        end) in
+        let fieldmap_to_field_list (map : InferredType.t FieldMap.t) =
+          FieldMap.bindings map
+          |> List.map (fun ((offset, size), ty) ->
+              ({ offset; size; ty } : InferredType.field))
         in
-        Function (name0, ins, outs0)
-  | _ -> Union (ty0, ty1)
-
-let merge_nodes (m : (bool * ty) automata) =
-  Hashtbl.iter
-    (fun (p, start_state) edges_tbl ->
-      let edges = Hashtbl.keys edges_tbl in
-      Iter.iter
-        (fun edge ->
-          let ads =
-            List.fold_left
-              (fun acc (p, head) ->
-                Hashtbl.remove edges_tbl edge;
-                join head acc)
-              Top
-              (Hashtbl.find_all edges_tbl edge)
+        let fieldmap_of_list (fields : InferredType.field list) :
+            InferredType.t FieldMap.t =
+          List.fold_left
+            (fun acc ({ offset; size; ty } : InferredType.field) ->
+              FieldMap.add (offset, size) ty acc)
+            FieldMap.empty fields
+        in
+        let f0 = fieldmap_of_list fields0 in
+        let f1 = fieldmap_of_list fields1 in
+        let joined_map =
+          FieldMap.merge_safe
+            ~f:(fun (offset, size) v ->
+              match v with
+              | `Both (a, b) -> Some (InferredType.Union (a, b))
+              | `Left a | `Right a -> Some a)
+            f0 f1
+        in
+        Record (fieldmap_to_field_list joined_map)
+    | Pointer (a, b), Pointer (c, d) ->
+        (* ptr((a u c) n (b n d), (b n d)) *)
+        Pointer (Sect (Union (a, c), Sect (b, d)), Sect (b, d))
+    (* WARN: this is not how BinSub did it, but I think I am just smarter and had better DS *)
+    | Function (name0, ins0, outs0), Function (name1, ins1, outs1) ->
+        if not @@ String.equal name0 name1 then failwith "BOOOOM"
+        else
+          (* args are the same just just union over the args *)
+          let ins =
+            StringMap.merge_safe
+              ~f:(fun _ b ->
+                match b with
+                | `Both (l, r) -> Some (InferredType.Sect (l, r))
+                | _ -> failwith "BOOOMM")
+              ins0 ins1
           in
-          m.states <- (p,ads) :: m.states;
-          Hashtbl.add edges_tbl edge (p, ads))
-        edges;
-      ())
-    m.transitions
+          Function (name0, ins, outs0)
+    | _ -> Union (ty0, ty1)
 
-let export_graphviz (n : 'a automata) =
-  Printf.sprintf
-    "\n\
-     digraph G {\n\
-    \ \"%s\" [height=0, width=0, style=filled, fillcolor=\"#c4a7e7\" ]\n\
-     %s\n\
-     \"%s\" -> %s;\n\
-     %s\n\
-     }"
-    n.name
-    (List.fold_left
-       (fun a s ->
-         let shape = if fst s then "house" else "invhouse" in
-         Printf.sprintf
-           "%s\"%s\" [shape=%s style=filled, fillcolor=\"#c4a7e7\"];\n" a
-           (show_ty @@ snd s)
-           shape)
-       "" n.states)
-    n.name
-    (Printf.sprintf "\"%s\"" @@ show_ty @@ snd n.start)
-    (List.fold_left
-       (fun acc ((_, s), a, (_, t)) ->
-         Printf.sprintf "%s\"%s\" -> \"%s\" [label=\"%s\", ];\n" acc (show_ty s)
-           (show_ty t)
-         @@ show_sigma a)
-       "" (get_transitions n))
+  let merge_nodes m =
+    Hashtbl.iter
+      (fun (p, start_state) edges_tbl ->
+        let edges = Hashtbl.keys edges_tbl in
+        Iter.iter
+          (fun edge ->
+            let ads =
+              List.fold_left
+                (fun acc (p, head) ->
+                  Hashtbl.remove edges_tbl edge;
+                  join head acc)
+                Top
+                (Hashtbl.find_all edges_tbl edge)
+            in
+            m.states <- (p, ads) :: m.states;
+            Hashtbl.add edges_tbl edge (p, ads))
+          edges;
+        ())
+      m.transitions
 
-let create_automata2 states transitions init fin name =
-  let length = List.length states in
-  let accepting = Hashtbl.create length in
-  List.iter (fun s -> Hashtbl.add accepting s (List.mem s fin)) states;
-  { states; transitions; start = init; name }
+  let export_graphviz n =
+    Printf.sprintf
+      "\n\
+       digraph G {\n\
+      \ \"%s\" [height=0, width=0, style=filled, fillcolor=\"#c4a7e7\" ]\n\
+       %s\n\
+       \"%s\" -> %s;\n\
+       %s\n\
+       }"
+      n.name
+      (List.fold_left
+         (fun a (polarity, typ) ->
+           let shape =
+             if Polarity.positive polarity then "house" else "invhouse"
+           in
+           Printf.sprintf
+             "%s\"%s\" [shape=%s style=filled, fillcolor=\"#c4a7e7\"];\n" a
+             (InferredType.show @@ typ) shape)
+         "" n.states)
+      n.name
+      (Printf.sprintf "\"%s\"" @@ InferredType.show @@ snd n.start)
+      (List.fold_left
+         (fun acc ((_, s), a, (_, t)) ->
+           Printf.sprintf "%s\"%s\" -> \"%s\" [label=\"%s\", ];\n" acc
+             (InferredType.show s) (InferredType.show t)
+           @@ Sigma.show a)
+         "" (get_transitions n))
+
+  let create_type_automata states transitions init fin name =
+    let length = List.length states in
+    let accepting = Hashtbl.create length in
+    List.iter (fun s -> Hashtbl.add accepting s (List.mem s fin)) states;
+    { states; transitions; start = init; name }
+end
