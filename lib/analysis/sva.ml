a2697565f36fbde8c676ceb9887625539d481091
JTrenerry <105094182+JTrenerry@users.noreply.github.com>
2026-02-18 13:52:13 +1000

    adds sym bases

--- lib/analysis/sva.ml
+++ lib/analysis/sva.ml
@@ -5,6 +5,42 @@ open Containers
 open Common
 open Wrapped_intervals
 module BVSet = Set.Make (Bitvec)
+module IntervalLatticeSet = Set.Make (WrappedIntervalsLattice)
+
+module SymBase = struct
+  type t =
+    | Stack of string
+    | Heap of { name : string; label : string }
+    | GlobSym of { interval : WrappedIntervalsLattice.t }
+    | Constant
+    | Par of { name : string; param_name : string }
+    | Ret of {
+        name : string;
+        target_name : string;
+        label : string;
+        param_name : string;
+      }
+    | Loaded of { name : string; label : string }
+
+  let show = function
+    | Stack name -> Printf.sprintf "Stack(%s)" name
+    | Heap { name; label } -> Printf.sprintf "Heap(%s_%s)" name label
+    | GlobSym _ -> "Global"
+    | Constant -> "Constant"
+    | Par { name; param_name } -> Printf.sprintf "Par(%s_%s)" name param_name
+    | Ret { name; target_name; label; param_name } ->
+        Printf.sprintf "Ret(%s_%s_%s_%s)" name target_name label param_name
+    | Loaded { name; label } -> Printf.sprintf "Loaded(%s_%s)" name label
+
+  let place_holder = function
+    | Stack _ | Heap _ | GlobSym _ | Constant -> false
+    | Ret _ | Par _ | Loaded _ -> true
+end
+
+module type Offsets = sig
+  val toOffsets : BVSet.t
+  val toIntervals : IntervalLatticeSet.t
+end
 
 module type OffsetDomain = sig
   type t
@@ -14,20 +50,39 @@ module type OffsetDomain = sig
   val should_widen : t -> bool
   val transform : t -> (Bitvec.t -> Bitvec.t) -> t
   val transform_t : t -> (t -> t) -> t
-  val add : t -> t -> bool -> t
+  val add : bool -> t -> t -> t
 end
 
 module IntervalDomain : OffsetDomain = struct
-  include WrappedIntervalsLattice
+  open WrappedIntervalsLattice
+
+  type t = WrappedIntervalsLattice.t
 
   let init i = interval i i
   let init_set set = interval (BVSet.min_elt set) @@ BVSet.max_elt set
-  let should_widen t = true
-  let transform t f = t
-  let transform_t t f = t
-  let add t t' neg = match (t, t') with
+  let should_widen t = false
+
+  let transform t f =
+    match t with
+    | Top | Bot -> t
+    | Interval { lower; upper } -> Interval { lower = f lower; upper = f upper }
+
+  let transform_t t f = f t
+
+  let add neg t t' =
+    match (t, t') with
     | Top, _ | _, Top -> Top
-    | _, _ -> Bot
+    | a, Bot | Bot, a -> a
+    | Interval { lower; upper }, Interval { lower = lower2; upper = upper2 } ->
+        let lower2, upper2 =
+          if neg then (Bitvec.neg lower2, Bitvec.neg upper2)
+          else (lower2, upper2)
+        in
+        let o1 = Bitvec.add lower upper2 in
+        let o2 = Bitvec.add lower lower2 in
+        let o3 = Bitvec.add upper upper2 in
+        let o4 = Bitvec.add upper lower2 in
+        init_set @@ BVSet.of_list [ o1; o2; o3; o4 ]
 end
 
 module SymAddrSetLattice = struct
