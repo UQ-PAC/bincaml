0302f0940e3276a3e96515ddbaca9f7d2b0a6415
rina <k@rina.fyi>
2026-02-18 14:24:45 +1000

    restarting lol

--- bin/lsp/main.ml
+++ bin/lsp/main.ml
@@ -1,270 +1,90 @@
-open Yojson
-open Lexing
-open Basilloader.BasilVisitor
-open Lwt.Infix
-open Lsp.Types
-open BasilIR
-open Basillang
-open Basillsp.Tokens
-open Basillsp.Semantic_tokens
-open Basillsp.Process_doc
-open Common.Util
+(* This file is free software, part of linol. See file "LICENSE" for more information *)
 
-let debug = true
-let oc = if debug then Some (open_out ".basillsplog") else None
+(* Some user code
 
-let log (s : string) =
-  Option.iter
-    (fun oc ->
-      output_string oc (s ^ "\n");
-      flush oc)
-    oc
+   The code here is just a placeholder to make this file compile, it is expected
+   that users have an implementation of a processing function for input contents.
 
-let showTree (t : AbsBasilIR.moduleT) : string =
-  "[Linearized tree]\n\n"
-  ^ PrintBasilIR.printTree PrintBasilIR.prtModuleT t
-  ^ "\n"
+   Here we expect a few things:
+   - a type to represent a state/environment that results from processing an
+     input file
+   - a function procdessing an input file (given the file contents as a string),
+     which return a state/environment
+   - a function to extract a list of diagnostics from a state/environment.
+     Diagnostics includes all the warnings, errors and messages that the processing
+     of a document are expected to be able to return.
+*)
 
-type doc_ast =
-  | SyntaxError of (string * position * position)
-  | Ast of AbsBasilIR.moduleT * Processor.symbs
+module Lsp = Linol.Lsp
 
-type state_after_processing = {
-  linebreaks : linebreaks;
-  ast : doc_ast;
-  procs : BasilAST.BasilAST.proc list;
-  graphs : (unit -> Cfg.procedure_rec) StringMap.t;
-}
+type state_after_processing = unit
 
-let process (s : string) : state_after_processing =
-  let lexbuf = Lexing.from_string s in
-  let linebreaks = linebreaks s in
-  try
-    let prog = ParBasilIR.pModuleT LexBasilIR.token lexbuf in
-    let syms = Processor.process_cast linebreaks prog in
-    let tokens = SemanticTokensProcessor.lex_tokens_of_string linebreaks s in
-    let syms =
-      {
-        syms with
-        all_tokens =
-          TokenMap.union (fun i a b -> Some a) syms.all_tokens tokens;
-      }
-    in
-    let procs =
-      try Ast_loader.ast_of_concrete_ast prog
-      with exn ->
-        let e = Printexc.to_string exn in
-        log (Printf.sprintf "%s:\n" e);
-        Option.iter Printexc.print_backtrace oc;
-        []
-    in
-    let graphs =
-      List.map
-        (fun (proc : BasilAST.BasilAST.proc) ->
-          (proc.label, fun () -> Cfg.graph_of_proc proc))
-        procs
-      |> StringMap.of_list
-    in
-    (*Processor.print_syms syms; *)
-    { linebreaks; ast = Ast (prog, syms); procs; graphs }
-  with ParBasilIR.Error ->
-    let start_pos = Lexing.lexeme_start_p lexbuf
-    and end_pos = Lexing.lexeme_end_p lexbuf in
-    {
-      linebreaks;
-      ast = SyntaxError (lexeme lexbuf, start_pos, end_pos);
-      procs = [];
-      graphs = StringMap.empty;
-    }
+let process_some_input_file (_file_contents : string) : state_after_processing =
+  ()
 
-let process_some_input_file (_file_contents : string) :
-    state_after_processing =
-  process _file_contents
+let diagnostics (_state : state_after_processing) : Lsp.Types.Diagnostic.t list
+    =
+  let pos = Lsp.Types.Position.create ~character:1 ~line:1 in
+  let range = Lsp.Types.Range.create ~end_:pos ~start:pos in
+  [
+    Lsp.Types.Diagnostic.create ~message:(`String "hi!!!!!!!!!") ~range ()
+  ]
 
-let diagnostics (_state : state_after_processing) :
-    Linol_lsp.Types.Diagnostic.t list =
-  match _state.ast with
-  | Ast _ -> []
-  | SyntaxError (l, p1, p2) ->
-      [
-        Linol_lsp.Types.Diagnostic.create
-          ~message:(`String ("Syntax error: '" ^ l ^ "'"))
-          ~range:(range_of_position _state.linebreaks p1 p2)
-          ~severity:Linol_lsp.Types.DiagnosticSeverity.Error ();
-      ]
+(* Lsp server class
 
-open Linol_lwt.Locations
-open Linol_lwt
+   This is the main point of interaction beetween the code checking documents
+   (parsing, typing, etc...), and the code of linol.
 
+   The [Linol_lwt.Jsonrpc2.server] class defines a method for each of the action
+   that the lsp server receives, such as opening of a document, when a document
+   changes, etc.. By default, the method predefined does nothing (or errors out ?),
+   so that users only need to override methods that they want the server to
+   actually meaningfully interpret and respond to.
+*)
 class lsp_server =
   object (self)
     inherit Linol_lwt.Jsonrpc2.server
 
-    val buffers
-        : (Linol_lsp.Types.DocumentUri.t, state_after_processing) Hashtbl.t =
+    (* one env per document *)
+    val buffers : (Lsp.Types.DocumentUri.t, state_after_processing) Hashtbl.t =
       Hashtbl.create 32
 
     method spawn_query_handler f = Linol_lwt.spawn f
-    method! config_definition = Some (`Bool true)
 
-    method! config_code_lens_options =
-      Some (Linol_lsp.Lsp.Types.CodeLensOptions.create ())
-
-    method! config_symbol =
-      Some
-        (`DocumentSymbolOptions
-           (DocumentSymbolOptions.create ~label:"symbols"
-              ~workDoneProgress:true ()))
-
+    (* We define here a helper method that will:
+       - process a document
+       - store the state resulting from the processing
+       - return the diagnostics from the new state
+    *)
     method private _on_doc ~(notify_back : Linol_lwt.Jsonrpc2.notify_back)
-        (uri : Linol_lsp.Types.DocumentUri.t) (contents : string) =
+        (uri : Lsp.Types.DocumentUri.t) (contents : string) =
       let new_state = process_some_input_file contents in
       Hashtbl.replace buffers uri new_state;
       let diags = diagnostics new_state in
       notify_back#send_diagnostic diags
 
+    (* We now override the [on_notify_doc_did_open] method that will be called
+       by the server each time a new document is opened. *)
     method on_notif_doc_did_open ~notify_back d ~content : unit Linol_lwt.t =
       self#_on_doc ~notify_back d.uri content
 
+    (* Similarly, we also override the [on_notify_doc_did_change] method that will be called
+       by the server each time a new document is opened. *)
     method on_notif_doc_did_change ~notify_back d _c ~old_content:_old
         ~new_content =
       self#_on_doc ~notify_back d.uri new_content
 
-    method on_req_hover ~notify_back ~id ~uri ~pos ~workDoneToken
-        (d : Linol_lwt.doc_state) =
-      Lwt.return None
-
-    method! on_req_code_lens_resolve ~notify_back ~id code_lens =
-      (* our code lenses are cheap so probably fine to resolve in one
-         stage *)
-      Lwt.return code_lens
-
-    method! config_list_commands = [ "show_cfg" ]
-
-    method! on_req_execute_command ~notify_back ~id ~workDoneToken cmd args =
-      (match args with
-      | Some [ uri; `String proc ] ->
-          Some (Linol_lsp.Uri0.t_of_yojson uri, proc)
-      | _ -> None)
-      |> Option.map (fun (uri, proc_label) ->
-             let state = Hashtbl.find buffers uri in
-             let pg = StringMap.find proc_label state.graphs in
-             let g = pg () in
-             Cfg.display_with_viewer g)
-      |> ignore;
-      Lwt.return (`String "ok")
-
-    method! on_req_code_lens ~notify_back ~id ~uri ~workDoneToken
-        ~partialResultToken ds =
-      let state = Hashtbl.find buffers uri in
-      let p =
-        state.procs
-        |> List.map (fun (p : BasilAST.BasilAST.proc) ->
-               let b, e = p.label_lexical_range |> Option.get in
-               let range =
-                 token_of_char_range state.linebreaks b e |> Token.to_range
-               in
-               let command : Linol_lsp.Types.Command.t =
-                 {
-                   arguments =
-                     Some [ Linol_lsp.Uri0.yojson_of_t uri; `String p.label ];
-                   command = "show_cfg";
-                   title = "Show CFG";
-                 }
-               in
-               let codelens =
-                 Linol_lsp.Types.CodeLens.create ~command ~range ()
-               in
-               codelens)
-      in
-      Lwt.return p
-
-    (* `Location of Location.t list *)
-    method on_req_definition ~notify_back ~id ~uri ~pos ~workDoneToken
-        ~partialResultToken d : Linol_lwt.Locations.t option Lwt.t =
-      log "req_definition ";
-      (match (Hashtbl.find buffers d.uri).ast with
-      | Ast (p, syms) ->
-          log
-            ("get definition : " ^ string_of_int pos.line ^ " "
-            ^ string_of_int pos.character);
-          Processor.find_definition syms pos.line pos.character
-      | _ -> None)
-      |> Option.map (fun def ->
-             `Location
-               [
-                 Linol_lwt.Location.create
-                   ~range:(Token.to_range def.label_tok)
-                   ~uri;
-               ])
-      |> Lwt.return
-
-    method on_req_symbol ~notify_back ~id ~uri ~workDoneToken
-        ~partialResultToken e =
-      log "req syms";
-      let r =
-        (match (Hashtbl.find buffers uri).ast with
-        | Ast (p, syms) -> Some syms
-        | _ -> None)
-        |> Option.map (fun s -> Processor.get_syms s)
-        |> Option.map (fun x -> `DocumentSymbol x)
-      in
-      (function Some x -> log "syms" | None -> log "no syms") r;
-      Lwt.return r
-
+    (* On document closes, we remove the state associated to the file from the global
+       hashtable state, to avoid leaking memory. *)
     method on_notif_doc_did_close ~notify_back:_ d : unit Linol_lwt.t =
       Hashtbl.remove buffers d.uri;
       Linol_lwt.return ()
-
-    method config_modify_capabilities (c : ServerCapabilities.t) :
-        ServerCapabilities.t =
-      {
-        c with
-        semanticTokensProvider =
-          Some
-            (`SemanticTokensRegistrationOptions
-               SemanticTokensProcessor.semantic_tokens_config);
-      }
-
-    method on_request_unhandled : type r.
-        notify_back:Linol_lwt.Jsonrpc2.notify_back ->
-        id:Linol_lwt.Jsonrpc2.Req_id.t ->
-        r Linol.Lsp.Client_request.t ->
-        r Linol_lwt.Jsonrpc2.IO.t =
-      fun ~notify_back:(_ : Linol_lwt.Jsonrpc2.notify_back) ~id:_ _r ->
-        match _r with
-        | Linol_lsp.Client_request.SemanticTokensRange
-            { partialResultToken; range; textDocument; workDoneToken } ->
-            log "semantic token range";
-            let begin_range : Token.t =
-              (LineCol.of_position range.start, 0)
-            in
-            let end_range : Token.t = (LineCol.of_position range.end_, -1) in
-            let m =
-              (match (Hashtbl.find buffers textDocument.uri).ast with
-              | Ast (p, syms) -> Some syms.all_tokens
-              | _ -> None)
-              |> Option.map (fun m ->
-                     match TokenMap.split begin_range m with
-                     | l, data, r -> r)
-              |> Option.map (fun m ->
-                     match TokenMap.split end_range m with l, data, r -> l)
-              |> Option.map SemanticTokensProcessor.to_semantic_tokens_full
-            in
-            Lwt.return m
-        | Linol_lsp.Client_request.SemanticTokensFull
-            { partialResultToken; textDocument; workDoneToken } ->
-            let uri = textDocument.uri in
-            (match (Hashtbl.find buffers uri).ast with
-            | Ast (p, syms) -> Some syms
-            | _ -> None)
-            |> Option.map Processor.to_semantic_highlight_data
-            |> fun i -> Lwt.return i
-        | _ -> Lwt.fail_with "TODO: handle this request"
   end
 
+(* Main code
+   This is the code that creates an instance of the lsp server class
+   and runs it as a task. *)
 let run () =
-  log "start";
   let s = new lsp_server in
   let server = Linol_lwt.Jsonrpc2.create_stdio ~env:() s in
   let task =
@@ -274,17 +94,9 @@ let run () =
   match Linol_lwt.run task with
   | () -> ()
   | exception e ->
-      let e = Printexc.to_string e in
-      Option.iter (fun oc -> Printf.fprintf oc "error: %s\n%!" e) oc;
-      exit 1
+    let e = Printexc.to_string e in
+    Printf.eprintf "error: %s\n%!" e;
+    exit 1
 
-let () =
-  Option.iter
-    (fun oc ->
-      Printexc.record_backtrace true;
-      Printexc.register_printer (function e ->
-          Some
-            (Printexc.print_backtrace oc;
-             "")))
-    oc;
-  run ()
+(* Finally, we actually run the server *)
+let () = run ()
