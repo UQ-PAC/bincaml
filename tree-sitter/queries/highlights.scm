f9d28b52e91fe143cca37e8ccdf7d361b9012ed0
rina <k@rina.fyi>
2026-02-19 17:07:24 +1000

    disable

--- tree-sitter/queries/highlights.scm
+++ tree-sitter/queries/highlights.scm
@@ -1,25 +1,25 @@
 (keyword) @keyword.operator
 (other) @keyword
-((other) @x
-  (#match? @x "^//.*")) @comment
-((other) @x
-  (#match? @x "^/\\*.*")) @comment
-((other) @x
-  (#match? @x "[^a-zA-Z_0-9]+")) @punctuation.delimiter
-((other) @x
-  (#match? @x "^(->|=|:=)$")) @operator
-((other) @x
-  (#match? @x "^[0-9]")) @number
-((other) @x
-  (#match? @x "^(int|bool|bv)")) @type.builtin
-((other) @x
-  (#match? @x "^[@%$#.]")) @variable
-((other) @x
-  (#match? @x "^(proc|prog)")) @keyword.function
-((other) @x
-  (#match? @x "^block")) @keyword.conditional
-
-((other) @x
-  (#match? @x "^(var|assert|assume|return|goto|load|store|unreachable)")) @keyword.return
-
-
+; ((other) @x
+;   (#match? @x "^//.*")) @comment
+; ((other) @x
+;   (#match? @x "^/\\*.*")) @comment
+; ((other) @x
+;   (#match? @x "[^a-zA-Z_0-9]+")) @punctuation.delimiter
+; ((other) @x
+;   (#match? @x "^(->|=|:=)$")) @operator
+; ((other) @x
+;   (#match? @x "^[0-9]")) @number
+; ((other) @x
+;   (#match? @x "^(int|bool|bv)")) @type.builtin
+; ((other) @x
+;   (#match? @x "^[@%$#.]")) @variable
+; ((other) @x
+;   (#match? @x "^(proc|prog)")) @keyword.function
+; ((other) @x
+;   (#match? @x "^block")) @keyword.conditional
+;
+; ((other) @x
+;   (#match? @x "^(var|assert|assume|return|goto|load|store|unreachable)")) @keyword.return
+;
+;
