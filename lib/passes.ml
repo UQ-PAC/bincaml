9e3ee2d7a81f5d4d02dc79ee8b1a892f903003bb
JTrenerry <105094182+JTrenerry@users.noreply.github.com>
2026-02-23 16:04:31 +1000

    fix merge

--- lib/passes.ml
+++ lib/passes.ml
@@ -106,7 +106,6 @@ module PassManager = struct
         "Runs wrapped interval analysis on control flow graph and prints \
          results";
     }
-
   let cfg_tnum_wint_reduced =
     {
       name = "demo-cfg-tnum-wint-reduced-analysis";
@@ -123,7 +122,13 @@ module PassManager = struct
   let sva =
     {
       name = "sva";
-      apply = DFGAnalysis (module Analysis.Sva.Analysis);
+      apply =
+        Proc
+          (fun p ->
+            let r = Analysis.Sva.analyse p in
+            (* Analysis.Wrapped_intervals.Analysis.print_dot *)
+              (* (Format.of_chan stdout) p r; *)
+            p);
       doc =
         "Runs symbolic value analysis and prints stuff out after";
     }
