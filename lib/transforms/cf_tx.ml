8e6ea74be49ce177137d05946b4c519495a8c73b
agle <a.michael1@uq.edu.au>
2026-02-23 17:40:45 +1000

    rw check

--- lib/transforms/cf_tx.ml
+++ lib/transforms/cf_tx.ml
@@ -4,7 +4,7 @@ open Bincaml_util.Common
 open Lang
 open Expr_eval
 
-let simplify_proc_exprs p =
+let simplify_proc_exprs ?visit p =
   let blocks = Procedure.blocks_to_list p in
 
   let open Procedure.Edge in
@@ -14,13 +14,72 @@ let simplify_proc_exprs p =
       | Procedure.Vert.Begin bid, (b : (Var.t, Expr.BasilExpr.t) Block.t) ->
           let stmts =
             Vector.map
-              (Stmt.map ~f_lvar:id ~f_rvar:id ~f_expr:Algsimp.alg_simp_rewriter)
+              (Stmt.map ~f_lvar:id ~f_rvar:id
+                 ~f_expr:(Algsimp.alg_simp_rewriter ?visit))
               b.stmts
           in
           Procedure.update_block p bid { b with stmts }
       | _ -> p)
     p blocks
 
-let simplify_prog_exprs (p : Program.t) =
-  let procs = ID.Map.map (fun proc -> simplify_proc_exprs proc) p.procs in
+let simplify_prog_exprs ?visit (p : Program.t) =
+  let procs =
+    ID.Map.map (fun proc -> simplify_proc_exprs ?visit proc) p.procs
+  in
   { p with procs }
+
+let to_smt (r : Expr.BasilExpr.rwinfo) =
+  let open Lang.Expr_smt in
+  let cexpr = Expr.BasilExpr.binexp ~op:`NEQ r.from r.into in
+  let smt = snd @@ SMTLib2.assert_bexpr cexpr SMTLib2.empty in
+  smt |> SMTLib2.to_sexp ~set_logic:false
+
+let online_check visit (solver : Bincaml_util.Solver.solver)
+    (r : Expr.BasilExpr.rwinfo) =
+  let open Bincaml_util in
+  let _ = solver.command (Solver.push 1) in
+  to_smt r |> Iter.iter (fun i -> ignore @@ solver.command i);
+  let res : Solver.result = Solver.check solver in
+  (match res with
+  | Sat ->
+      let m = Solver.get_model solver in
+      visit (Some m) r
+  | Unsat -> ()
+  | Unknown -> visit None r);
+  let r = solver.command (Solver.pop 1) in
+  ()
+
+let online_check_all visit rws =
+  let solver = Bincaml_util.Solver.new_solver Bincaml_util.Solver.cvc5 in
+  List.iter (online_check visit solver) rws;
+  solver.stop ()
+
+let print_error model (rw : Expr.BasilExpr.rwinfo) =
+  let m = model |> Option.map Sexp.to_string |> Option.get_or ~default:"" in
+  Printf.fprintf stdout "%s\n\n"
+  @@ "unsound rewrite "
+  ^ Option.get_or ~default:"" rw.__FILE__
+  ^ ":"
+  ^ Option.get_or ~default:"" (Option.map Int.to_string rw.__LINE__)
+  ^ "\n   "
+  ^ Expr.BasilExpr.show_rwinfo rw
+  ^ "\n" ^ m
+
+(*
+let write_smt_checks ls fname =
+  let smt = List.fold_left to_smt Lang.Expr_smt.SMTLib2.empty ls in
+  let sexp = Lang.Expr_smt.SMTLib2.to_sexp smt in
+  Sexp.to_file_iter fname sexp;
+  ()
+
+*)
+
+let simplify_prog_with_smt_check x =
+  let rewrites = ref [] in
+  let visit (x : Expr.BasilExpr.rwinfo) =
+    rewrites := x :: !rewrites;
+    ()
+  in
+  let prog = simplify_prog_exprs ~visit x in
+  online_check_all print_error !rewrites;
+  prog
