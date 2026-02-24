24a9ff9b9a34f4e116d67014fa6be1b5a962e9e2
bpaul <bpaul@bpaul.xyz>
2026-02-24 17:48:34 +1000

    avoid adding redundant requires with smt

--- lib/transforms/function_summaries.ml
+++ lib/transforms/function_summaries.ml
@@ -22,10 +22,24 @@ let wp_dual_requires (module S : FunctionSummaryAnnotation)
 
 (** `redundant p ps` returns true if the conjunction of `p :: ps` is equivalent
     to that of `ps`. *)
-let redundant p ps = false
+let redundant (solver : Bincaml_util.Smt.Solver.t) p ps =
+  let conj = Expr.BasilExpr.applyintrin ~op:`AND ps in
+  let q = Expr.BasilExpr.boolnot @@ Expr.BasilExpr.binexp ~op:`IMPLIES conj p in
+  let open Expr_smt in
+  let s =
+    SMTLib2.assert_bexpr q SMTLib2.empty
+    |> snd
+    |> SMTLib2.to_sexp ~set_logic:true
+  in
+  let open Bincaml_util.Smt in
+  let _ = Solver.push solver in
+  s |> Iter.iter (fun c -> ignore @@ Solver.add_command solver c);
+  let res = Solver.check solver in
+  let _ = Solver.pop solver in
+  match res with Unsat -> true | Sat -> false | Unknown -> false
 
-let annotate_proc (s : (module FunctionSummaryAnnotation)) (proc : Program.proc)
-    =
+let annotate_proc (solver : Bincaml_util.Smt.Solver.t)
+    (s : (module FunctionSummaryAnnotation)) (proc : Program.proc) =
   let r = wp_dual_requires s proc in
   (* TODO implement a sample ensures clause generator and some sort of analysis
      pass runner *)
@@ -33,20 +47,29 @@ let annotate_proc (s : (module FunctionSummaryAnnotation)) (proc : Program.proc)
     (fun proc r ->
       let spec = Procedure.specification proc in
       let requires =
-        if redundant r spec.requires then spec.requires else r :: spec.requires
+        if redundant solver r spec.requires then spec.requires
+        else r :: spec.requires
       in
       let spec = { spec with requires } in
       Procedure.set_specification proc spec)
     proc r
 
 let transform =
-  annotate_proc
+  let solver =
+    Bincaml_util.Smt.Solver.create
+      {
+        Bincaml_util.Smt.Config.cvc5 with
+        log = Bincaml_util.Smt.Config.quiet_log;
+      }
+  in
+  annotate_proc solver
     (module struct
       let requires _ = []
       let ensures _ = []
     end : FunctionSummaryAnnotation)
 
-let annotate_component _g (prog : Program.t) component =
+let annotate_component (solver : Bincaml_util.Smt.Solver.t) _g
+    (prog : Program.t) component =
   (* TODO will want to use an smt solver to perform leq checks then do a data
        flow analysis on the component, but for now can just do a linear pass *)
   let open Program.CallGraph.Vert in
@@ -57,7 +80,7 @@ let annotate_component _g (prog : Program.t) component =
         | ProcBegin pid ->
             let proc = ID.Map.find pid procs in
             let proc' =
-              annotate_proc
+              annotate_proc solver
                 (module struct
                   let requires id =
                     ID.Map.find id procs |> Procedure.specification |> fun s ->
@@ -78,4 +101,11 @@ let annotate_component _g (prog : Program.t) component =
 let interproc_transform (prog : Program.t) =
   let call_graph = Program.CallGraph.make_call_graph prog in
   let sccs = Program.CallGraph.Scc.scc_list call_graph in
-  List.fold_left (annotate_component call_graph) prog sccs
+  let solver =
+    Bincaml_util.Smt.Solver.create
+      {
+        Bincaml_util.Smt.Config.cvc5 with
+        log = Bincaml_util.Smt.Config.quiet_log;
+      }
+  in
+  List.fold_left (annotate_component solver call_graph) prog sccs
