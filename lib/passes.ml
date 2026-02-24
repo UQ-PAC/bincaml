f900d2527dc3d46966668a65284f3aecf10df39e
JTrenerry <105094182+JTrenerry@users.noreply.github.com>
2026-02-24 12:29:52 +1000

    adds fucntions to LatticeMap and adjust passes for cfg

--- lib/passes.ml
+++ lib/passes.ml
@@ -125,8 +125,8 @@ module PassManager = struct
         Proc
           (fun p ->
             let r = Analysis.Sva.analyse p in
-            (* Analysis.Wrapped_intervals.Analysis.print_dot *)
-              (* (Format.of_chan stdout) p r; *)
+            Analysis.Sva.Analysis.print_dot
+              (Format.of_chan stdout) p r;
             p);
       doc =
         "Runs symbolic value analysis and prints stuff out after";
