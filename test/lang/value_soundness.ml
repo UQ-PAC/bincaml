149a32d76f0fc1637bf5c34619869aa3bb9d7b38
JTrenerry <105094182+JTrenerry@users.noreply.github.com>
2026-03-02 14:20:32 +1000

    testing for type inference

--- test/lang/value_soundness.ml
+++ test/lang/value_soundness.ml
@@ -107,10 +107,8 @@ module TestTnumWintReducedDom =
   ValueAbstractionSoundness
     (Analysis.Tnum_wint_reduced_product
      .TnumWintReducedProductValueAbstractionBasil)
-     
-module TestSVADom =
-  ValueAbstractionSoundness
-    (Analysis.Sva.SVAAbstractionBasil)
+
+module TestSVADom = ValueAbstractionSoundness (Analysis.Sva.SVAAbstractionBasil)
 
 let _ =
   Alcotest.run "value domain abstract eval soundness"
