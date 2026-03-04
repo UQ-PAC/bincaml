3f9ed5ca8a34a4b78e5bb97a0c1dd84a2b428654
JTrenerry <105094182+JTrenerry@users.noreply.github.com>
2026-03-04 15:26:36 +1000

    fmt

--- lib/analysis/sva.ml
+++ lib/analysis/sva.ml
@@ -46,7 +46,7 @@ module SymBase = struct
 
   let to_int = Hashtbl.hash
   let pretty a = Containers_pp.int 1
-  let is_stack = function | Stack _ -> true | _ -> false  
+  let is_stack = function Stack _ -> true | _ -> false
 end
 
 module IntervalDomain = struct
