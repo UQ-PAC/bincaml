38208a0cac5f8b74c723ca540ca83a845b1371a9
rina <k@rina.fyi>
2026-03-08 20:13:23 +1000

    Tim McGilchrist has saved us

--- flake.nix
+++ flake.nix
@@ -26,10 +26,17 @@
             selfOcamlPackages = pkgs.ocamlPackages.overrideScope (ofinal: oprev: {
 
               # https://github.com/NixOS/nixpkgs/blob/aca4d95fce4914b3892661bcb80b8087293536c6/pkgs/development/compilers/ocaml/generic.nix#L30
-              # ocaml = (oprev.ocaml.override {
-              #   flambdaSupport = true;
-              #   framePointerSupport = true;
-              # }).overrideAttrs  { doCheck = false; };
+              ocaml = (oprev.ocaml.override {
+                flambdaSupport = true;
+                framePointerSupport = true;
+              }).overrideAttrs (ocaml: {
+                patches = ocaml.patches ++ [
+                  (pkgs.fetchpatch {
+                    url = "https://github.com/ocaml/ocaml/commit/c2eec4dd1de7d0da2d2f76e5e7f2b567901f4e2c.patch";
+                    hash = "sha256-qDx8saOLhFMYaK4PLsSvHnDBYKvRSMmPtdVa/IqkQSI=";
+                  })
+                ];
+              });
 
               bincaml = ofinal.callPackage ./nix/bincaml.nix { };
               hector = ofinal.callPackage ./nix/hector.nix { };
