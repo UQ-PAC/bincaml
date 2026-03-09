c8df4105e0a8b25251d77dbef547c93d3f76591d
katrinafyi <39479354+katrinafyi@users.noreply.github.com>
2026-03-09 11:57:11 +1000

    Nix: nixpkgs-based flake, instead of dune-/opam-nix (#79)
    Closes https://github.com/agle/bincaml/issues/54
    
    This uses the nixpkgs buildDunePackage (https://ryantm.github.io/nixpkgs/languages-frameworks/ocaml/), instead of opam-nix or dune-nix. This is a lot simpler and it gives good separate caching of dependencies and still lets you use ocaml compiler options. However, it's fully manual and you'll always need to add the dependencies to bincaml.nix manually.
    
    I've tried it out in `nix develop` and it seems to work well. `dune build` can build the project and successfully finds the dependencies installed by nix. I've also set up some environment variables which are needed to make odig work inside the nix shell.

---
+++ nix/shell.nix
@@ -0,0 +1,49 @@
+{ lib
+, stdenv
+, mkShell
+
+# ocaml packages
+, bincaml
+, odoc
+, odig
+, ocaml-lsp
+, ocamlformat
+
+# dev packages
+, tree-sitter
+, nodejs-slim
+, perf
+
+}:
+
+mkShell {
+  packages = [
+    odoc odig ocaml-lsp ocamlformat
+    tree-sitter nodejs-slim
+    # sherlodoc
+  ] ++ lib.optional stdenv.hostPlatform.isLinux perf;
+
+  inputsFrom = [
+    bincaml
+  ];
+
+  shellHook = ''
+    export ODIG_CACHE_DIR=~/.cache/odig
+
+    ocaml_hash="$(echo "$OCAMLPATH" | sha1sum | cut -d' ' -f1)"
+    if [[ -z "$ocaml_hash" ]]; then
+      echo 'cannot make ocaml hash - cannot cache odig'
+      export ODIG_LIB_DIR="$(mktemp -d)/lib"
+    else
+      export ODIG_LIB_DIR="$ODIG_CACHE_DIR/$ocaml_hash"
+    fi
+
+    if ! [[ -d "$ODIG_LIB_DIR" ]]; then
+      mkdir -p "$ODIG_LIB_DIR"
+      IFS=':' read -ra ADDR <<< "$OCAMLPATH"
+      for i in "''${ADDR[@]}"; do
+        ln -sf $i/* "$ODIG_LIB_DIR"
+      done
+    fi
+  '';
+}
