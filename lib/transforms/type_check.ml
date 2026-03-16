f0d63c7139ad2b9082cf00591fc0ac4435347d35
McArthur-Alford <mcarthur.alford@proton.me>
2026-03-17 09:42:57 +1000

    Cleaned up type checking

--- lib/transforms/type_check.ml
+++ lib/transforms/type_check.ml
@@ -170,28 +170,26 @@ let type_check stmt_id block_id expr =
                        ty ))
                  ([], h) tl)
     | `MapUpdate -> (
-        let arg0 = List.hd args in
-        let arg1 = List.nth args 1 in
-        let arg2 = List.nth args 2 in
-        match List.hd args with
-        | Types.Map (k, v) ->
-            []
-            @
-            if Types.equal k arg1 then []
+        match args with
+        | [ Types.Map (k, v); arg1; arg2 ] ->
+            let err =
+              if Types.equal k arg1 then []
+              else
+                [
+                  type_err "map update expected key type %s but got %s"
+                    (Types.to_string k) (Types.to_string arg1);
+                ]
+            in
+            if Types.equal v arg2 then err
             else
-              [
-                type_err "map update expected key type %s but got %s"
-                  (Types.to_string k) (Types.to_string arg1);
-              ]
-              @ if Types.equal v arg2 then [] else [
-                type_err "map update expected value type %s but got %s"
-                  (Types.to_string v) (Types.to_string arg2);
-              ]
-        | _ ->
-            [
-              type_err "%s is not of map type in %s" (Types.to_string arg0)
+              type_err "map update expected value type %s but got %s"
+                (Types.to_string v) (Types.to_string arg2)
+              :: err
+        | [ a; b; c ] ->[
+              type_err "%s is not of map type in %s" (Types.to_string a)
                 (Ops.AllOps.to_string op);
-            ])
+            ]
+        | _ -> [ type_err "map update expects 3 arguments" ])
   in
 
   let type_error_alg e =
