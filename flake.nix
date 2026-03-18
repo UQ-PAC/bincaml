d33258fe0ae303957c5bb2630860529cda2b6047
rina <k@rina.fyi>
2026-03-18 17:02:42 +1000

    nixfmt :(((((((((

--- flake.nix
+++ flake.nix
@@ -5,22 +5,29 @@
     infuse-src.flake = false;
   };
   outputs =
-    { self
-    , nixpkgs
-    , infuse-src
+    {
+      self,
+      nixpkgs,
+      infuse-src,
     }@args:
     let
       inherit (nixpkgs) lib;
 
-      inherit (import ./nix/infuse-lib.nix {
-        lib = lib;
-        infuse-src = infuse-src;
-      }) infuse infuse-with;
+      inherit
+        (import ./nix/infuse-lib.nix {
+          lib = lib;
+          infuse-src = infuse-src;
+        })
+        infuse
+        infuse-with
+        ;
 
       inherit (import ./nix/flake-for-all-systems.nix { lib = lib; })
-        flake-for-all-systems;
+        flake-for-all-systems
+        ;
 
-    in flake-for-all-systems args {
+    in
+    flake-for-all-systems args {
       overlays = {
         addBincamlPackages = ofinal: _: {
           bincaml = ofinal.callPackage ./nix/bincaml.nix { };
@@ -28,27 +35,36 @@
           intPQueue = ofinal.callPackage ./nix/intpqueue.nix { };
         };
 
-        enableOcamlFramePointer = ofinal: infuse-with {
-          # https://github.com/NixOS/nixpkgs/blob/aca4d95fce4914b3892661bcb80b8087293536c6/pkgs/development/compilers/ocaml/generic.nix#L30
-          ocaml.__input.flambdaSupport.__assign = true;
-          ocaml.__input.framePointerSupport.__assign = true;
-          ocaml.__attrs.patches.__append = [
-            (ofinal.callPackage ({ fetchpatch }: fetchpatch {
-              url = "https://github.com/ocaml/ocaml/commit/c2eec4dd1de7d0da2d2f76e5e7f2b567901f4e2c.patch";
-              hash = "sha256-qDx8saOLhFMYaK4PLsSvHnDBYKvRSMmPtdVa/IqkQSI=";
-            }) { })
-          ];
-        };
+        enableOcamlFramePointer =
+          ofinal:
+          infuse-with {
+            # https://github.com/NixOS/nixpkgs/blob/aca4d95fce4914b3892661bcb80b8087293536c6/pkgs/development/compilers/ocaml/generic.nix#L30
+            ocaml.__input.flambdaSupport.__assign = true;
+            ocaml.__input.framePointerSupport.__assign = true;
+            ocaml.__attrs.patches.__append = [
+              (ofinal.callPackage (
+                { fetchpatch }:
+                fetchpatch {
+                  url = "https://github.com/ocaml/ocaml/commit/c2eec4dd1de7d0da2d2f76e5e7f2b567901f4e2c.patch";
+                  hash = "sha256-qDx8saOLhFMYaK4PLsSvHnDBYKvRSMmPtdVa/IqkQSI=";
+                }
+              ) { })
+            ];
+          };
       };
 
-      systems = ["aarch64-linux" "x86_64-linux" "aarch64-darwin" "x86_64-darwin"];
-      perSystem = { self, nixpkgs, ... }:
+      systems = [
+        "aarch64-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        { self, nixpkgs, ... }:
         let
           pkgs = nixpkgs.legacyPackages;
-          selfOcamlPackages =
-            pkgs.ocamlPackages.overrideScope self.overlays.addBincamlPackages;
-          fpOcamlPackages =
-            selfOcamlPackages.overrideScope self.overlays.enableOcamlFramePointer;
+          selfOcamlPackages = pkgs.ocamlPackages.overrideScope self.overlays.addBincamlPackages;
+          fpOcamlPackages = selfOcamlPackages.overrideScope self.overlays.enableOcamlFramePointer;
         in
         {
           defaultPackage = selfOcamlPackages.bincaml;
@@ -68,8 +84,6 @@
             fp = fpOcamlPackages.callPackage ./nix/shell.nix { };
             no-fp = selfOcamlPackages.callPackage ./nix/shell.nix { };
           };
-        }
-      ;
-    }
-  ;
+        };
+    };
 }
