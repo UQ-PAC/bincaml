c369103c64687c95e873e109beab3cd37a3f60dd
bpaul <bpaul@bpaul.xyz>
2026-03-19 14:51:19 +1000

    test promote

--- lib/analysis/ide_live.ml
+++ lib/analysis/ide_live.ml
@@ -324,10 +324,13 @@ proc @fun2(f:bv64, global_in:bv64)  -> (out2:bv64) {  }
     {|
     @fun2
     (L,L->IdEdge), (L,f->ConstEdge true), (L,f_2->ConstEdge true), (L,f_1->ConstEdge true), (out2,global_in->IdEdge), (out2,out2->IdEdge), (out2,global_1->IdEdge), (out2,g_2->IdEdge), (out2,g_1->IdEdge), (out2,f_3->IdEdge), (out2,g_3->IdEdge)
+    global_in, f, out2, global_1, f_2, g_2, f_1, g_1, f_3, g_3
     @main
     (L,L->IdEdge), (L,b->ConstEdge true), (L,global_in->ConstEdge true), (L,y->ConstEdge true), (L,global_1->ConstEdge true), (L,a->ConstEdge true), (L,x->ConstEdge true), (L,b_1->ConstEdge true), (L,x_1->ConstEdge true), (L,y_1->ConstEdge true)
+    b, global_in, y, global_1, a, x, b_1, x_1, y_1
     @fun1
     (L,L->IdEdge), (L,d->ConstEdge true), (out,global_in->IdEdge), (out,c->IdEdge), (out,out->IdEdge), (out,global_1->IdEdge), (out,e->IdEdge)
+    global_in, c, d, out, global_1, e
     |}]
 
 let ide_test (p : Program.t) =
