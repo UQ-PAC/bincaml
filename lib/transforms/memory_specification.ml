b5879f94c6793713d03deae27286a04d57eac56a
McArthur-Alford <mcarthur.alford@proton.me>
2026-03-23 16:29:51 +1000

    not much really

--- lib/transforms/memory_specification.ml
+++ lib/transforms/memory_specification.ml
@@ -1,5 +1,27 @@
+open Lang
+open Lang.Common
+open Lang.Expr
+open Ops
+
 (* specifies memory? *)
 (* memory_encoding produces the memory encoding declarations for a particular implementation *)
 (* memory_specification uses the encoding to specify memory, adding in asserts/assumes and procedure specifications *)
 
-let transform p = p
+let transform_proc (p : Program.proc) =
+  let name = ID.name (Procedure.id p) in
+  match name with
+  | "@malloc" ->
+      Procedure.set_specification p
+        {
+          requires = [];
+          ensures = List.repeat 10 [BasilExpr.boolconst true];
+          rely = [];
+          guarantee = [];
+          captures_globs = [];
+          modifies_globs = [];
+        }
+  | _ -> p
+
+let transform (p : Program.t) =
+  let procs = ID.Map.map transform_proc p.procs in
+  { p with procs }
