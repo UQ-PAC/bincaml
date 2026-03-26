c82c96bf9c23ab6d53526a7ceb1aefb385a7b30a
b-paul <bpaul@bpaul.xyz>
2026-03-26 16:35:30 +1000

    Requires clause generation with WP dual domain (#70)
    * Initial impl
    
    * fix simplify
    
    * more simps
    
    * Write preconditions
    
    * leq
    
    * use new expr records in matches
    
    * support interproc wpdual
    
    * Interproc summary gen
    
    * Collapse gammas of exprs into the vars
    
    * new rewriter
    
    * avoid adding redundant requires with smt
    
    * fixpoint on sccs attempt 1
    
    * correctly fix on sccs
    
    * Remove old comments
    
    * inter and intra transforms
    
    * cleanup
    
    * test
    
    * fmt
    
    * remove print
    
    * keep
    
    * oops
    
    * pass documentation
    
    * simp
    
    * Move pointless first class modules
    
    * Join of gammas is and, not or!

--- lib/transforms/cf_tx.ml
+++ lib/transforms/cf_tx.ml
@@ -76,8 +76,8 @@ let to_smt (r : Expr.BasilExpr.rwinfo) =
 let online_check visit (solver : Bincaml_util.Smt.Solver.t)
     (r : Expr.BasilExpr.rwinfo) =
   let open Bincaml_util.Smt in
-  let _ = Solver.push solver in
-  to_smt r |> Iter.iter (fun i -> ignore @@ Solver.add_command solver i);
+  Solver.push solver;
+  to_smt r |> Iter.iter (fun i -> Solver.add_command solver i);
   let res : Solver.result = Solver.check solver in
   (match res with
   | Sat ->
@@ -86,7 +86,7 @@ let online_check visit (solver : Bincaml_util.Smt.Solver.t)
       visit (Some (from, into)) r
   | Unsat -> ()
   | Unknown -> visit None r);
-  let _ = Solver.pop solver in
+  Solver.pop solver;
   ()
 
 let online_check_all ?(debug = false) visit rws =
