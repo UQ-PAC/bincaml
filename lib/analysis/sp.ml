fe9e2e2d5f5a6acf2a227771f057acd85f56e9ae
McArthur-Alford <mcarthur.alford@proton.me>
2026-05-05 16:18:14 +1000

    added vertex to init

--- lib/analysis/sp.ml
+++ lib/analysis/sp.ml
@@ -15,7 +15,7 @@ end
 module Domain (S : FunctionAnnotation) = struct
   let name = "SP domain"
 
-  type t = Program.e option
+  type t = Program.e
 
   let e_true = BasilExpr.boolconst true
   let e_false = BasilExpr.boolconst false
@@ -72,23 +72,13 @@ module Domain (S : FunctionAnnotation) = struct
     in
     rw % rw
 
-  let unwrap p = Option.get_or ~default:e_true p
-  let show = Option.map_or ~default:"" BasilExpr.to_string
-  let equal = Option.equal BasilExpr.equal
-  let compare = Option.compare BasilExpr.compare
-  let pretty = BasilExpr.pretty % unwrap
-  let top = None
-  let bottom = Some e_false
-
-  let join (a : t) (b : t) : t =
-    let o =
-      match (a, b) with
-      | None, None -> Some e_true
-      | None, x | x, None -> x
-      | Some a, Some b -> Some (BasilExpr.applyintrin ~op:`OR [ a; b ])
-    in
-    o
-
+  let show = BasilExpr.to_string
+  let equal = BasilExpr.equal
+  let compare = BasilExpr.compare
+  let pretty = BasilExpr.pretty
+  let top = e_true
+  let bottom = e_false
+  let join (a : t) (b : t) : t = BasilExpr.applyintrin ~op:`OR [ a; b ]
   let leq a b = failwith "leq not implemented"
   let widening a b = top
 
@@ -116,71 +106,61 @@ module Domain (S : FunctionAnnotation) = struct
     |> simplify
 
   let transfer (p : t) (stmt : Program.stmt) : t =
-    let p = unwrap p in
-    let o =
-      match stmt with
-      | Instr_Assign [] -> p
-      | Instr_Assign (a : (Var.t * BasilExpr.t) list) ->
-          tf_assigns p a |> simplify
-      | Instr_Assume { body; branch = false } ->
-          conjunction [ p; simplify body ]
-      | Instr_Assert { body } -> conjunction [ p; simplify body ]
-      | Instr_Call { lhs; procid; args } ->
-          let sub (s : BasilExpr.t) : BasilExpr.t =
-            s
-            |> substitute_var_names (StringMap.to_list args)
-            |> substitute_var_names
-                 (StringMap.to_list lhs
-                 |> List.map (fun (i, v) -> (i, BasilExpr.rvar v)))
-          in
-          let r =
-            conjunction @@ (e_true :: (S.ensures procid |> List.map sub))
-          in
-          conjunction [ p; simplify r ]
-      | Instr_Store
-          {
-            lhs;
-            rhs;
-            value;
-            addr = Addr { addr : 'e; size : int; endian : Stmt.endian };
-          } ->
-          conjunction
-            [
-              p;
-              BasilExpr.binexp ~op:`EQ value
-                (BasilExpr.binexp
-                   ~op:(`Load (endian, size))
-                   (BasilExpr.rvar lhs) addr);
-            ]
-      | Instr_Load
-          {
-            lhs;
-            rhs;
-            addr = Addr { addr : 'e; size : int; endian : Stmt.endian };
-          } ->
-          (* Without a load expression it is unclear what to constrain here. *)
-          (* Could be a map access in future. *)
-          e_true
-      | Instr_IndirectCall _ | Instr_IntrinCall _ -> e_true
-      | _ -> e_true
-    in
-    Some o
+    match stmt with
+    | Instr_Assign [] -> p
+    | Instr_Assign (a : (Var.t * BasilExpr.t) list) ->
+        tf_assigns p a |> simplify
+    | Instr_Assume { body; branch = false } -> conjunction [ p; simplify body ]
+    | Instr_Assert { body } -> conjunction [ p; simplify body ]
+    | Instr_Call { lhs; procid; args } ->
+        let sub (s : BasilExpr.t) : BasilExpr.t =
+          s
+          |> substitute_var_names (StringMap.to_list args)
+          |> substitute_var_names
+               (StringMap.to_list lhs
+               |> List.map (fun (i, v) -> (i, BasilExpr.rvar v)))
+        in
+        let r = conjunction @@ (e_true :: (S.ensures procid |> List.map sub)) in
+        conjunction [ p; simplify r ]
+    | Instr_Store
+        {
+          lhs;
+          rhs;
+          value;
+          addr = Addr { addr : 'e; size : int; endian : Stmt.endian };
+        } ->
+        conjunction
+          [
+            p;
+            BasilExpr.binexp ~op:`EQ value
+              (BasilExpr.binexp
+                 ~op:(`Load (endian, size))
+                 (BasilExpr.rvar lhs) addr);
+          ]
+    | Instr_Load
+        {
+          lhs;
+          rhs;
+          addr = Addr { addr : 'e; size : int; endian : Stmt.endian };
+        } ->
+        (* Without a load expression it is unclear what to constrain here. *)
+        (* Could be a map access in future. *)
+        e_true
+    | Instr_IndirectCall _ | Instr_IntrinCall _ -> e_true
+    | _ -> e_true
 
-  let init ?(vertex=None) (proc : Program.proc) : t = top
+  let init ?(vertex = None) (proc : Program.proc) : t =
+    match vertex with Some Procedure.Vert.Entry -> top | _ -> bottom
 
   let transfer_phi (m : t) (p : Var.t Block.phi) : t =
-    let m = unwrap m in
     match p with
     | { lhs; rhs } ->
         let rhs =
-          List.map
-            (fun (_, v) -> Some (tf_assigns m [ (lhs, BasilExpr.rvar v) ]))
-            rhs
+          List.map (fun (_, v) -> tf_assigns m [ (lhs, BasilExpr.rvar v) ]) rhs
         in
         List.fold_left join top rhs
 
   let to_pred (p : t) : BasilExpr.t =
-    let p = unwrap p in
     let l = locals p in
     match VarSet.cardinal l with
     | 0 -> p
