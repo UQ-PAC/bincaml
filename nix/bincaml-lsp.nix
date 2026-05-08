33874aa258e7edd85d5b550a326690cbcd47ddab
JTrenerry <105094182+JTrenerry@users.noreply.github.com>
2026-05-08 10:29:41 +1000

    Fixes lsp flake to provide some more packages that are used for tests

--- nix/bincaml-lsp.nix
+++ nix/bincaml-lsp.nix
@@ -1,22 +1,23 @@
-{ lib
-, buildDunePackage
-, nix-gitignore
-, writableTmpDirAsHomeHook
+{
+  lib,
+  buildDunePackage,
+  nix-gitignore,
+  writableTmpDirAsHomeHook,
 
-# ocaml packages
-, bincaml
-, logs
-, fmt
-, iter
-, linol
-, linol-lwt
-, containers
-, ppx_deriving
-, ppx_import
+  # ocaml packages
+  bincaml,
+  logs,
+  fmt,
+  iter,
+  linol,
+  linol-lwt,
+  containers,
+  ppx_deriving,
+  ppx_import,
 
-# test:
+  # test:
 
-# dev:
+  # dev:
 }:
 
 buildDunePackage {
@@ -27,14 +28,27 @@ buildDunePackage {
 
   src = nix-gitignore.gitignoreSource [ "nix" "flake.nix" "flake.lock" ] ./..;
 
-  checkInputs = [];
+  checkInputs = [ ];
   nativeBuildInputs = [ writableTmpDirAsHomeHook ];
-  buildInputs =
-    [ bincaml logs fmt iter linol linol-lwt containers ppx_deriving ppx_import ];
-  propagatedBuildInputs = [ ];
+  buildInputs = [
+    bincaml
+    logs
+    fmt
+    iter
+    containers
+    ppx_deriving
+  ];
+  propagatedBuildInputs = [
+    linol
+    linol-lwt
+    ppx_import
+  ];
 
   doCheck = true;
-  outputs = [ "out" "dev" ];
+  outputs = [
+    "out"
+    "dev"
+  ];
 
   meta = {
     homepage = "https://github.com/agle/bincaml";
