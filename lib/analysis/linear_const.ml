46756f0e5b1e9a5319d65d17fc428a9f97da9802
bpaul <bpaul@bpaul.xyz>
2026-06-11 15:02:31 +1000

    Fix join

--- lib/analysis/linear_const.ml
+++ lib/analysis/linear_const.ml
@@ -72,10 +72,13 @@ module LF = struct
   (* Definitions from https://doi.org/10.1016/0304-3975(96)00072-2 computing inverses mod 2^n *)
 
   let compute_join a b c d =
+    (* This computes the join of ax + b and cx + d
+       According to the paper we can compute this as ax + b join a * l + b, where l = (b-d)/(c-a),
+       and we can compute this only when c-a has an inverse mod 2^n, i.e. only when c-a is odd (as hence it is coprime to 2^n) *)
     let bd = Bitvec.value (Bitvec.sub b d) in
     let ca = Bitvec.value (Bitvec.sub c a) in
     let pow = Z.pow (Z.of_int 2) (Bitvec.size a) in
-    if Z.congruent ca Z.one pow then
+    if Z.is_odd ca then
       let cainv = Z.invert ca pow in
       let l0 = Bitvec.create ~size:(Bitvec.size a) (Z.mul cainv bd) in
       let j = Bitvec.add (Bitvec.mul a l0) b in
