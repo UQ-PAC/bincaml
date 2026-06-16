7951720a1627fe238a87fe768b8dd27bc32533f4
rina <k@rina.fyi>
2026-06-16 16:33:05 +1000

    update tests and update IBI with new types

--- test/transforms/test_aslp.ml
+++ test/transforms/test_aslp.ml
@@ -29,7 +29,7 @@ proc @main()  -> () {  }
     |}
   in
   let prog = lst.prog in
-  let proc = Lang.Program.entry_proc_exn prog in
+  let _proc = Lang.Program.entry_proc_exn prog in
   print_endline
   @@ Containers_pp.Pretty.to_string ~width:80 (Lang.Program.prog_pretty prog);
   [%expect
@@ -40,31 +40,20 @@ proc @main()  -> () {  }
       requires boolor(eq(0x400808:bv64, $PC))
 
     [
-       block %main_code [
+       block %main_code { .gtirb_block = "b8tsihT4Q6a/SWPo4w8HoA";
+           .succ = [ { .address = 4196228; .conditional = "false"; .direct = "true";
+                   .target = "stmts:OuTzy8qRTci75taVjGinFQ"; .type = "Type_Call" } ] } [
          assume eq(0x400808:bv64, $PC);
-         assert false;
-         assert false;
-         assert false;
-         assert false;
-         assert false;
-         assert false;
+         assert false { .opcode = "0xa9be7bfd" };
+         assert false { .opcode = "0x910003fd" };
+         assert false { .opcode = "0xb9001fe0" };
+         assert false { .opcode = "0xf9000be1" };
+         assert false { .opcode = "0xb9801fe0" };
+         assert false { .opcode = "0x97ffffda" };
          assert boolor(eq(0x400784:bv64, $PC));
          goto (%ret_1);
        ];
        block %ret_1 [ return; ]
     ];
     prog entry @main;
-  |}];
-  Lang.Procedure.iter_stmt_topo_fwd proc
-  |> Iter.iter (fun stmt ->
-      Printf.printf "%b\n" (StringMap.is_empty (Lang.Stmt.attrib stmt)));
-  [%expect {|
-    true
-    true
-    true
-    true
-    true
-    true
-    true
-    true
     |}]
