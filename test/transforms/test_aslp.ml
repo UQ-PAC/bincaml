86e44e16ea68d14bbd579b883ab3413e7af653c5
rina <k@rina.fyi>
2026-06-19 18:27:38 +1000

    update tests after module restructure

--- test/transforms/test_aslp.ml
+++ test/transforms/test_aslp.ml
@@ -11,10 +11,10 @@ let%expect_test "lift empty" =
   print_endline @@ Aslp_state.show_aslp_state x;
   [%expect
     {|
-    { Aslp.Aslp_state.blocks = "block_0"
-      -> { Aslp.Aslp_state.assume = None; stmts = []; succs = ["block_1"] },
-      "block_1" -> { Aslp.Aslp_state.assume = None; stmts = []; succs = [] };
-      entry = "block_0"; exit = "block_1" }
+    { Aslp_state.blocks = "block_0"
+      -> { Aslp_state.assume = None; stmts = []; succs = ["block_1"] }, "block_1"
+      -> { Aslp_state.assume = None; stmts = []; succs = [] }; entry = "block_0";
+      exit = "block_1" }
     |}]
 
 let%expect_test "lift: add x1, x2, x3, lsl #4" =
@@ -29,13 +29,13 @@ let%expect_test "lift: add x1, x2, x3, lsl #4" =
   print_endline @@ Aslp_state.show_aslp_state x;
   [%expect
     {|
-    { Aslp.Aslp_state.blocks = "block_0"
-      -> { Aslp.Aslp_state.assume = None;
+    { Aslp_state.blocks = "block_0"
+      -> { Aslp_state.assume = None;
            stmts =
            [var var_0:bv64 := v__R2:bv64; var var_1:bv64 := v__R3:bv64;
              var v__R1:bv64 := bvadd(var_0:bv64, bvshl(var_1:bv64, 0x4:bv12))];
            succs = ["block_1"] },
-      "block_1" -> { Aslp.Aslp_state.assume = None; stmts = []; succs = [] };
+      "block_1" -> { Aslp_state.assume = None; stmts = []; succs = [] };
       entry = "block_0"; exit = "block_1" }
     |}]
 
@@ -51,15 +51,14 @@ let%expect_test "lift 2x: mov x1, #0xabcd" =
   print_endline @@ Aslp_state.show_aslp_state x;
   [%expect
     {|
-    { Aslp.Aslp_state.blocks = "block_0"
-      -> { Aslp.Aslp_state.assume = None;
-           stmts = [var v__R1:bv64 := 0xabcd:bv64]; succs = ["block_1"] },
-      "block_1"
-      -> { Aslp.Aslp_state.assume = None; stmts = []; succs = ["block_2"] },
+    { Aslp_state.blocks = "block_0"
+      -> { Aslp_state.assume = None; stmts = [var v__R1:bv64 := 0xabcd:bv64];
+           succs = ["block_1"] },
+      "block_1" -> { Aslp_state.assume = None; stmts = []; succs = ["block_2"] },
       "block_2"
-      -> { Aslp.Aslp_state.assume = None;
-           stmts = [var v__R1:bv64 := 0xabcd:bv64]; succs = ["block_3"] },
-      "block_3" -> { Aslp.Aslp_state.assume = None; stmts = []; succs = [] };
+      -> { Aslp_state.assume = None; stmts = [var v__R1:bv64 := 0xabcd:bv64];
+           succs = ["block_3"] },
+      "block_3" -> { Aslp_state.assume = None; stmts = []; succs = [] };
       entry = "block_0"; exit = "block_3" }
     |}]
 
