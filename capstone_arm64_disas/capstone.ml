d59902d8ff97919d1a8c99d4d1325158fb29e1c8
agle <a.michael1@uq.edu.au>
2026-06-22 17:30:29 +1000

    fix test

--- capstone_arm64_disas/capstone.ml
+++ capstone_arm64_disas/capstone.ml
@@ -10,13 +10,13 @@ let disas_arm64_op (opcode : Int32.t) : (string, string) result =
 
 let%expect_test "invalid" =
   Result.iter_error print_endline (disas_arm64_op @@ Int32.of_int 0x123444);
-  [%expect {| add x1, x2, x3, lsl #4 |}]
+  [%expect {| capstone disassembly error |}]
 
 let%expect_test "invalid" =
   try print_endline (disas_arm64_op_ffi "ab")
   with Failure m ->
     print_endline m;
-    [%expect {| add x1, x2, x3, lsl #4 |}]
+    [%expect {| disas requries 4-byte buffer |}]
 
 let%expect_test "disas add" =
   Result.iter print_endline (disas_arm64_op @@ Int32.of_int 0x8b031041);
