7abe7056f89bfd4dab35b7300b146f57713f11ca
Kait <39479354+katrinafyi@users.noreply.github.com>
2026-07-01 08:14:22 +0000

    Revert "run on load-gtirb. add mem automatically. TODO: fix unsupported?"
    This reverts commit 66e0cd60fa4961ccd409fa9aa2da44e9b7998412.

--- lib/transforms/aslp/aslp_lexpr.ml
+++ lib/transforms/aslp/aslp_lexpr.ml
@@ -79,10 +79,6 @@ let to_var x =
 let pc_var = to_var PC
 let branchtaken_var = to_var BranchTaken
 
-let mem_var =
-  Var.create "$mem" ~scope:GlobalVarShared
-    Type.(Map (Bitvector 64, Bitvector 8))
-
 let predefined =
   [
     PC; SP_EL0;
