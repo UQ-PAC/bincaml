a065b1c84a20cd57e9a035d11bbe0c81ca4ccfdc
rina <k@rina.fyi>
2026-07-09 15:13:56 +1000

    isolate_stmts_of_block probably works now

--- lib/transforms/aslp/aslp_util_internal.ml
+++ lib/transforms/aslp/aslp_util_internal.ml
@@ -36,16 +36,48 @@ let referenced_vars_of_prog =
 (** Isolates statements satisfying the given predicate into their own block,
     while maintaining sequential control-flow between them. *)
 let isolate_stmts_of_block ?(label = "%singleton") ~proc f bid =
-  let b = Procedure.get_block proc bid |> Option.get_exn_or "block not found" in
-  let stmts = b.stmts |> CCVector.to_list in
+  let stmts =
+    Procedure.get_block proc bid |> Option.get_exn_or "block not found"
+    |> fun b -> CCVector.to_list b.stmts
+  in
 
-  let block_stmts =
+  (* Group, then flatten isolated statements into their own list. *)
+  let (grouped_stmts : _ Stmt.t list list) =
     List.group_succ ~eq:(CCFun.compose_binop f Bool.equal) stmts
     |> List.flat_map (function
       | hd :: _ as xs when f hd -> List.map List.pure xs
       | xs -> List.pure xs)
   in
-  2
+
+  (* Insert disconnected blocks for each group, reusing [initial_bid] for the first group. *)
+  let proc, block_ids =
+    List.fold_map_i
+      (fun proc i stmts ->
+        let proc, bid =
+          if i = 0 then
+            ( Procedure.modify_block proc bid (fun b ->
+                  { b with stmts = CCVector.of_list stmts }),
+              bid )
+          else Procedure.fresh_block proc ~name:label ~stmts ()
+        in
+        (proc, bid))
+      proc grouped_stmts
+  in
+
+  let proc =
+    match List.last_opt block_ids with
+    | Some to_ -> Procedure.transplant_outgoing_edges proc ~from:bid ~to_
+    | None -> proc
+  in
+
+  match block_ids with
+  | [] -> proc
+  | hd :: tl ->
+      List.fold_left
+        (fun (proc, from) next ->
+          (Procedure.add_goto proc ~from ~targets:[ next ], next))
+        (proc, hd) tl
+      |> fst
 
 (** Maps each statement inside the given block ID into a sequence of blocks,
     then links those blocks sequentially in control-flow. *)
