db3a69b8ff432b6c8517de59d0852d84e06f4055
Nambat <nphan25806@gmail.com>
2026-07-24 15:44:34 +1000

    Resolved conflicts with main branch

--- lib/analysis/highest_live_bit_simple.ml
+++ lib/analysis/highest_live_bit_simple.ml
@@ -15,8 +15,7 @@ open Idessi
     shifts and extracts are performed on a variable. *)
 
 (*
-  KNOWN ISSUE: A variable that is given to a call as a formal-in parameter is either not being detected in the analysis, or is given
-               BOT instead of TOP
+  KNOWN ISSUE: Lambdas rarely, or never, trigger in transfer_call
 *)
 
 (* Should run ide_live before this *)
