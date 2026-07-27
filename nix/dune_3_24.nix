4a8ca741b6ec7731b48ba4717074b976cbe766f9
rina <k@rina.fyi>
2026-07-27 17:32:06 +1000

    use release tarball

--- nix/dune_3_24.nix
+++ nix/dune_3_24.nix
@@ -1,14 +1,12 @@
 {
   dune_3,
-  fetchFromGitHub,
+  fetchurl,
 }:
 
 dune_3.overrideAttrs {
   version = "3.24.1";
-  src = fetchFromGitHub {
-    owner = "ocaml";
-    repo = "dune";
-    rev = "3.24.1";
-    hash = "sha256-hks62JafKmKdkRGTLTVV8c5/OZQYmDtT4L+4JeCPxfc=";
+  src = fetchurl {
+    url = "https://github.com/ocaml/dune/releases/download/3.24.1/dune-3.24.1.tbz";
+    hash = "sha256-Co6qYt/LlFgCvK+abyAmylIoMz7jkaG97dPnCj8m6iw=";
   };
 }
