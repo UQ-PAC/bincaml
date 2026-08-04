b437c928c569ab9725fb1e3a2ca764da45881e6b
rina <k@rina.fyi>
2026-08-04 15:24:54 +1000

    nix: add `.#bincamlDocs.docs` to run `odoc_driver` using nix
    this turns the documentation site into a Nix package, so we can
    (eventually) simplify the CI/CD for doc generation.
    
    you can test it with:
    
        nix build .#bincamlDocs.docs -Lv
    
    at the moment, it just builds the docs for the `fmt` package to keep it
    fast and small, but when it's done, it will build all the bincaml docs -
    same as the current github workflow.
    
    this involved packaging some odoc packages which were missing from
    nixpkgs. it also involves some careful folder structure construction and
    some faking, because the `odoc_driver` is built around the assumption of
    having opam switches.
    
    at the moment, it all works *aside from* it miscategorises the builtin
    packages (like `unix`) as separate opam packages.
    
    future work: sherlodoc

---
+++ nix/bincaml-docs.nix
@@ -0,0 +1,151 @@
+/**
+  Facilitates generating ocamldocs for the Bincaml package and its dependencies.
+*/
+{
+  lib,
+  newScope,
+}:
+
+lib.makeScope newScope (
+  self:
+  let
+    callPackage = self.callPackage;
+  in
+  {
+    /**
+      Usually, `odoc_driver` uses opam to discover packages, but we don't have
+      a real opam switch in Nix. Fortunately, `odoc_driver` also has the ability
+      to load packages from Dune's `_build` folder structure, implemented in the
+      `dune_overrides` function:
+        https://github.com/ocaml/odoc/blob/v3.1/src/driver/opam.ml#L228
+
+      This derivation transforms the separate Nix packages for Bincaml and its
+      dependencies into this Dune-like folder structure expected by `odoc_driver`.
+    */
+    fake_dune_prefix = callPackage (
+      {
+        ocaml,
+        buildEnv,
+        fmt,
+        yojson_2,
+        bos,
+      }:
+      (buildEnv {
+        name = "fake-dune-prefix-for-odoc";
+        paths = [
+          fmt # TODO: change to bincaml and bincaml_lsp
+        ];
+        includeClosures = true;
+        ignoreCollisions = false;
+        pathsToLink = [
+          "/share/doc"
+          "/lib/ocaml/${ocaml.version}/site-lib"
+        ];
+        postBuild = ''
+          d=$out/_build/install/default
+
+          mkdir -p $d $out/bin
+          mv -v $out/share/doc $d/doc
+          mv -v $out/lib/ocaml/*/site-lib $d/lib
+
+          mkdir $d/lib/ocaml
+          ln -s ${ocaml}/lib/ocaml/* $d/lib/ocaml
+
+          # this makes the builtin packages (like `unix`) appear
+          # as distinct top-level packages rather than being under
+          # `ocaml`. we probably need to fake `.changes` to avoid this.
+          for meta in ${ocaml}/lib/ocaml/*/META; do
+            ln -s $(dirname $meta) $d/lib
+          done
+
+          rm -rf $out/share $out/lib
+          rm -rf $d/lib/{compiler-libs,stdlib,topfind,stublibs}
+
+          cat <<EOF > $out/bin/activate_fake_dune_prefix.sh
+          export OCAMLPATH=$d/lib
+          EOF
+          chmod +x $out/bin/*
+        '';
+      }).overrideAttrs
+        (
+          _: prev: {
+            # `linol` (and others?) depend on hardcoded `yojson_2` which leads
+            # to both yojson 3 and 2 in the closure, which causes conflicts.
+            buildCommand = ''
+              grep -v ${yojson_2} $extraPathsFrom > without_yojson
+              extraPathsFrom=without_yojson
+              ${prev.buildCommand}
+            '';
+          }
+        )
+    ) { };
+
+    /**
+      Provides a no-op `opam` script which prints a path to an empty dir.
+    */
+    fake_opam = callPackage (
+      {
+        writeShellScriptBin,
+        emptyDirectory,
+      }:
+      (writeShellScriptBin "opam" "echo ${emptyDirectory}").overrideAttrs { name = "fake-opam-for-odoc"; }
+    ) { };
+
+    /**
+      Main output which builds the HTML/JS/CSS for the documentation website.
+
+      Also has a `dev` output which contains the `.odocl` files for Sherlodoc.
+    */
+    docs = callPackage (
+      {
+        stdenvNoCC,
+        emptyDirectory,
+        fake_opam,
+        fake_dune_prefix,
+        ocaml,
+        findlib,
+        odoc,
+        sherlodoc,
+        odoc-driver,
+      }:
+      stdenvNoCC.mkDerivation {
+        name = "bincaml-docs";
+
+        dontUnpack = true;
+        doCheck = true;
+
+        nativeBuildInputs = [
+          ocaml
+          findlib
+          odoc
+          odoc-driver
+          sherlodoc
+          fake_opam
+          fake_dune_prefix
+        ];
+
+        outputs = [ "out" "dev" ];
+
+        buildPhase = ''
+          source activate_fake_dune_prefix.sh
+          odoc_driver \
+            --html-dir=$out \
+            --mld-dir=$dev --odoc-dir=$dev --odocl-dir=$dev \
+            $(cd $OCAMLPATH && echo *)
+
+          ocamlfind printconf
+
+          echo ${fake_opam}
+          echo ${fake_dune_prefix}
+        '';
+
+        checkPhase = ''
+          [[ -f $out/ocaml/stdlib/index.html ]] || {
+            echo "stdlib index.html missing. stdlib links are probably broken."
+            exit 1
+          }
+        '';
+      }
+    ) { };
+  }
+)
