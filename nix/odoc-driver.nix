5a3931318b51cb900f09729631baa60e91d2087c
rina <k@rina.fyi>
2026-08-04 15:24:54 +1000

    nix: add `.#bincamlDocs.docs` to run `odoc_driver` using nix
    this turns the documentation site into a Nix package, so we can
    (eventually) simplify the CI/CD for doc generation.
    
    you can test it with:
    
        nix build .#bincamlDocs.docs -Lv
    
    at the moment, it just builds the docs for the `fmt` package to keep it
    fast and small, but when it's done, it will build all the bincaml docs -
    same as the current github workflow.
    
    this involved packaging some odoc packages which were missing from
    nixpkgs. it also involves some careful folder structure construction and
    some faking, because the `odoc_driver` is built around the assumption of
    having opam switches.
    
    at the moment, it all works *aside from* it miscategorises the builtin
    packages (like `unix`) as separate opam packages.
    
    future work: sherlodoc

---
+++ nix/odoc-driver.nix
@@ -0,0 +1,68 @@
+{
+  lib,
+  buildDunePackage,
+
+  odoc,
+  odoc-md,
+  fpath,
+  bos,
+  yojson,
+  findlib,
+  opam-format,
+  logs,
+  eio_main,
+  eio,
+  progress,
+  cmdliner,
+  sexplib,
+  ppx_sexp_conv,
+  sherlodoc,
+}:
+
+buildDunePackage {
+  pname = "odoc-driver";
+  inherit (odoc) version src;
+
+  nativeBuildInputs = [ sherlodoc ];
+  buildInputs = [
+    odoc
+    odoc-md
+    fpath
+    bos
+    yojson
+    findlib
+    opam-format
+    logs
+    eio_main
+    eio
+    progress
+    cmdliner
+    sexplib
+    ppx_sexp_conv
+  ];
+
+  postPatch = ''
+    substituteInPlace src/driver/ocamlfind.ml \
+      --replace-fail '~config ~env_camllib' "" \
+      --replace-fail 'let config =' 'let _config =' \
+      --replace-fail 'let env_camllib =' 'let _env_camllib ='
+
+    substituteInPlace src/driver/ocamlfind.ml --replace-fail 'let libname_of_archive =' 'let libname_of_archive =
+    (fun x ->
+      Fpath.Map.dump Format.pp_print_text Format.err_formatter x;
+      x) @@'
+  '';
+
+  nativeCheckInputs = [ ];
+  checkInputs = [ ];
+  doCheck = true;
+
+  meta = {
+    description = "OCaml Documentation Generator - Driver";
+    mainProgram = "odoc_driver";
+    license = lib.licenses.isc;
+    maintainers = [ lib.maintainers.katrinafyi ];
+    homepage = "https://github.com/ocaml/odoc";
+    changelog = "https://github.com/ocaml/odoc/blob/${odoc.version}/CHANGES.md";
+  };
+}
