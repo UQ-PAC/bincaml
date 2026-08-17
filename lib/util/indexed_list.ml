73d63df453d9266e8dd3c166278e58443debca9e
rina <k@rina.fyi>
2026-08-17 20:16:56 +1000

    fqueue
    earlier bugs:
    - insert_list_before with duplicate inserted keys
    - update that maps None to Some will have incomplete `order` list

--- lib/util/indexed_list.ml
+++ lib/util/indexed_list.ml
@@ -1,35 +1,49 @@
 (** Sequential and associative datastructure with fast iteration, appending, and
-    O(logn) indexing. *)
+    O(logn) indexing.
+
+    Basically, this can be used when you want an associative map but with a
+    manually-specified iteration order. *)
 
 open Containers
+open Containers.Fun
+
+open struct
+  module FQ = struct
+    include CCFQueue
+
+    (** Prepends the given list to the given queue. *)
+    let cons_l l q = List.fold_right CCFQueue.cons l q
+  end
+end
 
 module Make (K : Mtypes.ORD_TYPE) = struct
+  module S = Set.Make (K)
   module M = Map.Make (K)
 
-  (* Order stored reverse *)
-  type 'a t = { order : K.t list; values : 'a M.t }
-  (** invariant : \forall x . x \in order == x \in M.keys values *)
+  type 'a t = { order : K.t FQ.t; values : 'a M.t }
+  (** invariant : \forall x . x \in order == x \in M.keys values
 
-  let empty = { order = []; values = M.empty }
-  let singleton k v = { order = [ k ]; values = M.singleton k v }
+      [order] must be a permutation of the keys of [values]. *)
 
-  let of_iter m =
-    let m = Iter.persistent m in
-    let values = M.of_iter m in
-    (* we store in reverse order  *)
-    let order = m |> Iter.map fst |> Iter.to_rev_list in
-    (* no duplicate keys  *)
-    assert (Int.equal (M.cardinal values) (List.length order));
-    { values; order }
+  let empty = { order = FQ.empty; values = M.empty }
+  let singleton k v = { order = FQ.singleton k; values = M.singleton k v }
 
+  (** [append k v m] replaces [k -> v] in m, in-place if it is already defined.
+      Otherwise it is appended. This operation is the fastest insertion
+      operation (O(1)). *)
+  let append k v { order; values } =
+    if M.mem k values then { order; values = M.add k v values }
+    else { order = FQ.snoc order k; values = M.add k v values }
+
+  let of_iter m = Iter.fold (fun m (k, v) -> append k v m) empty m
   let of_list m = List.to_iter m |> of_iter
 
   (** Get in-order iterator of keys *)
-  let keys { order } = List.to_iter order |> Iter.rev
+  let keys { order } = FQ.to_iter order
 
   (** Get in-order iterator of key-value pairs *)
   let to_iter { order; values } =
-    List.to_iter order |> Iter.rev |> Iter.map (fun k -> (k, M.find k values))
+    FQ.to_iter order |> Iter.map (fun k -> (k, M.find k values))
 
   (** Get in-order list of pairs of keys and values *)
   let to_list m = m |> to_iter |> Iter.to_list
@@ -40,13 +54,6 @@ module Make (K : Mtypes.ORD_TYPE) = struct
   (** Values iterator out-of-order *)
   let values_nondet { values } = M.values values
 
-  (** [append k v m] replaces k -> v in m, in-place if it is already defined.
-      Otherwise it is appended. This operation is the fastest insertion
-      operation (O(1)). *)
-  let append k v { order; values } =
-    if M.mem k values then { order; values = M.add k v values }
-    else { order = k :: order; values = M.add k v values }
-
   (** [add k v m] replaces k -> v in m, in-place if it is already defined.
       Otherwise it is appended. This operation is the fastest insertion
       operation (O(1)). Identical to {!append}*)
@@ -54,17 +61,15 @@ module Make (K : Mtypes.ORD_TYPE) = struct
 
   let prepend k v { order; values } =
     if M.mem k values then { order; values = M.add k v values }
-    else { order = order @ [ k ]; values = M.add k v values }
+    else { order = FQ.cons k order; values = M.add k v values }
 
   let insert_before ~before k v { order; values } =
     if M.mem k values then { order; values = M.add k v values }
     else
-      let order = List.rev order in
-      let idx =
-        List.find_index (fun k -> before k (M.find k values)) order
-        |> Option.get_or ~default:(-1)
+      let before, after =
+        FQ.take_front_while (fun k -> not (before k (M.find k values))) order
       in
-      let order = List.insert_at_idx idx k order |> List.rev in
+      let order = FQ.cons_l before (FQ.cons k after) in
       { order; values = M.add k v values }
 
   let insert_before_key ~before k v m =
@@ -75,28 +80,27 @@ module Make (K : Mtypes.ORD_TYPE) = struct
   let insert_at_index ~before_index k v { order; values } =
     if M.mem k values then { order; values = M.add k v values }
     else
-      let idx = List.length order - before_index in
-      let order = List.insert_at_idx idx k order in
+      let before, after = FQ.take_front_l before_index order in
+      let order = FQ.cons_l before (FQ.cons k after) in
       { order; values = M.add k v values }
 
   (** Relatively efficient: cons *)
   let append_list news m = List.fold_left (fun acc (k, v) -> add k v acc) m news
 
-  let insert_list_before ~before news m =
-    let { values; order } = m in
-    let order = List.rev order in
-    let idx =
-      List.find_index (fun k -> before k (M.find k values)) order
-      |> Option.get_or ~default:(-1)
+  let insert_list_before ~before news { values; order } =
+    (* ignore pre-existing keys and only use leftmost position of each new key. *)
+    let _, new_keys =
+      let old_keys_set = S.of_iter (Iter.map fst (M.to_iter values)) in
+      List.fold_filter_map
+        (fun seen k -> (S.add k seen, if S.mem k seen then None else Some k))
+        old_keys_set (List.map fst news)
     in
-    let add_ord =
-      List.filter (fun (id, _) -> not @@ M.mem id values) news |> List.map fst
+
+    let before, after =
+      FQ.take_front_while (fun k -> before k (M.find k values)) order
     in
-    let pre, post = List.take_drop idx order in
-    let order = List.rev @@ pre @ add_ord @ post in
-    (* update all values  *)
-    let values = List.fold_left (fun acc (k, v) -> M.add k v acc) values news in
-    { order; values }
+    let order = FQ.cons_l before (FQ.cons_l new_keys after) in
+    { order; values = M.add_list values news }
 
   (** Lookup key, throwing [Not_found] if it does not exists. *)
   let find k { values } = M.find k values
@@ -120,30 +124,25 @@ module Make (K : Mtypes.ORD_TYPE) = struct
   let remove k { order; values } =
     let values' = M.remove k values in
     let order =
-      if not @@ Equal.physical values' values then
-        List.remove_one ~eq:K.equal k order
+      if not (Equal.physical values' values) then
+        order |> FQ.to_iter |> Iter.filter (not % K.equal k) |> FQ.of_iter
       else order
     in
     { order; values = values' }
 
-  let update k f { order; values } =
-    match f (M.find_opt k values) with
-    | Some new_val -> { order; values = M.add k new_val values }
-    | None ->
-        let order = List.remove_one ~eq:K.equal k order in
-        let values = M.remove k values in
-        { order; values }
+  let update k f m =
+    match f (find k m) with
+    | Some new_val -> add k new_val m
+    | None -> remove k m
 
-  (** Sort keys: just List.sort *)
+  (** Re-orders keys using a comparison function on keys. *)
   let sort_by_keys compar { order; values } =
-    let order = List.sort (Ord.opp compar) order in
+    let order = order |> FQ.to_list |> List.sort compar |> FQ.of_list in
     { order; values }
 
-  (** less efficient sort on values; copies index *)
-  let sort (compar : (K.t * 'a) Ord.t) ({ order; values } : 'a t) =
-    let ord a b =
-      Ord.opp (Ord.map (fun k -> (k, M.find k values)) compar) a b
-    in
-    let order = List.sort ord order in
+  (** Re-orders keys using a comparison function on `(key, value)`. *)
+  let sort (compar : (K.t * 'a) Ord.t) { order; values } =
+    let cmp = Ord.opp (Ord.map (fun k -> (k, M.find k values)) compar) in
+    let order = order |> FQ.to_list |> List.sort cmp |> FQ.of_list in
     { order; values }
 end
