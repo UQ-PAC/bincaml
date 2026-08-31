eb2fbfa545c5a70d4520f7082929bc592a1b38dd
bpaul <bpaul@bpaul.xyz>
2026-08-31 16:10:32 +1000

    domain impl

--- lib/analysis/lattice_collections.ml
+++ lib/analysis/lattice_collections.ml
@@ -196,8 +196,20 @@ module LatticeMap (K : MapKey) (V : TopLattice) = struct
         | TopMap m -> (`Top, KM.to_list m)
 
       let mapi f = function
-        | BotMap m -> BotMap (KM.mapi f m)
-        | TopMap m -> TopMap (KM.mapi f m)
+        | BotMap m ->
+            BotMap
+              (KM.filter_map
+                 (fun k v ->
+                   let v = f k v in
+                   if V.equal v V.bottom then None else Some v)
+                 m)
+        | TopMap m ->
+            TopMap
+              (KM.filter_map
+                 (fun k v ->
+                   let v = f k v in
+                   if V.equal v V.top then None else Some v)
+                 m)
 
       let fold f m acc =
         match m with BotMap m -> KM.fold f m acc | TopMap m -> KM.fold f m acc
