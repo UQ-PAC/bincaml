31870112322ad5006cea873dfa4290d7ef6b6620
bpaul <bpaul@bpaul.xyz>
2026-09-23 14:36:07 +1000

    test

--- test/transforms/test_branch_conditions.ml
+++ test/transforms/test_branch_conditions.ml
@@ -362,3 +362,110 @@ prog entry @main;
     ];
     prog entry @main;
     |}]
+
+let%expect_test "ccmp identification" =
+  let lst =
+    Loader.Loadir.ast_of_string
+      {|
+memory shared $mem : (bv64 -> bv8);
+var $PC:bv64;
+prog entry @main {.invariants = ["GtirbArm"]};
+
+proc @main()  -> () {  }
+[
+    block %main_code [
+    assume eq(0x00:bv64, $PC);
+    call @_aarch64_eval(0xeb04007f:bv32, 0x000:bv64) { .asm = "cmp x3, x4" };
+    call @_aarch64_eval(0xfa4610a4:bv32, 0x004:bv64) { .asm = "ccmp x5, x6, #4, ne" };
+    // rip it doesn't handle chained ccmps...
+    //call @_aarch64_eval(0xf148c0eb:bv32, 0x008:bv64) { .asm = "ccmp x7, x8, #11, gt" };
+    call @_aarch64_eval(0x9a822020:bv32, 0x008:bv64) { .asm = "csel x0, x1, x2, cs" };
+
+    goto (%ret_1);
+  ];
+  block %ret_1 [ return; ]
+];
+    |}
+  in
+  let prog =
+    lst.prog |> Transforms.Aslp.transform_program
+    |> Program.map_procedures (fun _ -> annotate_assume_flags)
+  in
+  print_endline
+  @@ Containers_pp.Pretty.to_string ~width:200 (Lang.Program.prog_pretty prog);
+  [%expect
+    {|
+    EQ {z = (Diff ($R3, $R4))}
+    var $R0:bv64;
+    var $R1:bv64;
+    var $R2:bv64;
+    var $R3:bv64;
+    var $R4:bv64;
+    var $R5:bv64;
+    var $R6:bv64;
+    var $PSTATE_N:bv1;
+    var $PSTATE_Z:bv1;
+    var $PSTATE_C:bv1;
+    var $PSTATE_V:bv1;
+    var observable $mem:(bv64->bv8);
+    var $PC:bv64;
+    proc @main()  -> () {  }
+      modifies $PC:bv64, $PSTATE_C:bv1, $PSTATE_N:bv1, $PSTATE_V:bv1, $PSTATE_Z:bv1, $R0:bv64
+      captures $PC:bv64, $PSTATE_C:bv1, $PSTATE_N:bv1, $PSTATE_V:bv1, $PSTATE_Z:bv1, $R0:bv64, $R1:bv64, $R2:bv64, $R3:bv64, $R4:bv64, $R5:bv64, $R6:bv64
+
+    [
+       block %main_code [ assume eq(0x0:bv64, $PC); goto (%block); ];
+       block %block { .asm = "cmp x3, x4" } [
+         var local:bv64 := 0x0:bv64;
+         var local_1:bv64 := 0x0:bv64;
+         $PSTATE_V:bv1 := bvnot(booltobv1(eq(sign_extend(64, bvadd(bvadd($R3, bvnot(bvshl($R4, zero_extend(52, 0x0:bv12)))), 0x1:bv64)),
+            bvadd(bvadd(sign_extend(64, $R3), sign_extend(64, bvnot(bvshl($R4, zero_extend(52, 0x0:bv12))))), 0x1:bv128))));
+         $PSTATE_C:bv1 := bvnot(booltobv1(eq(zero_extend(64, bvadd(bvadd($R3, bvnot(bvshl($R4, zero_extend(52, 0x0:bv12)))), 0x1:bv64)),
+            bvadd(bvadd(zero_extend(64, $R3), zero_extend(64, bvnot(bvshl($R4, zero_extend(52, 0x0:bv12))))), 0x1:bv128))));
+         $PSTATE_Z:bv1 := booltobv1(eq(bvadd(bvadd($R3, bvnot(bvshl($R4, zero_extend(52, 0x0:bv12)))), 0x1:bv64), 0x0:bv64));
+         $PSTATE_N:bv1 := extract(64,63, bvadd(bvadd($R3, bvnot(bvshl($R4, zero_extend(52, 0x0:bv12)))), 0x1:bv64));
+         (var BranchTaken:bool := false, $PC:bv64 := 0x4:bv64);
+         goto (%block_1);
+       ];
+       block %block_1 { .asm = "ccmp x5, x6, #4, ne" } [ var local_2:bool := false; var local_2:bool := eq($PSTATE_Z, 0x1:bv1); goto (%block_3,%block_2); ];
+       block %block_2 [
+         assume boolnot(local_2:bool) { .flag_semantics_$PSTATE_C = "(C (Diff ($R3, $R4)))"; .flag_semantics_$PSTATE_N = "(N (Diff ($R3, $R4)))"; .flag_semantics_$PSTATE_V = "(V (Diff ($R3, $R4)))";
+             .flag_semantics_$PSTATE_Z = "(Z (Diff ($R3, $R4)))" };
+         $PSTATE_V:bv1 := bvnot(booltobv1(eq(sign_extend(64, bvadd(bvadd($R5, bvnot($R6)), 0x1:bv64)), bvadd(bvadd(sign_extend(64, $R5), sign_extend(64, bvnot($R6))), 0x1:bv128))));
+         $PSTATE_C:bv1 := bvnot(booltobv1(eq(zero_extend(64, bvadd(bvadd($R5, bvnot($R6)), 0x1:bv64)), bvadd(bvadd(zero_extend(64, $R5), zero_extend(64, bvnot($R6))), 0x1:bv128))));
+         $PSTATE_Z:bv1 := booltobv1(eq(bvadd(bvadd($R5, bvnot($R6)), 0x1:bv64), 0x0:bv64));
+         $PSTATE_N:bv1 := extract(64,63, bvadd(bvadd($R5, bvnot($R6)), 0x1:bv64));
+         goto (%block_4);
+       ];
+       block %block_3 [
+         assume boolnot(boolnot(local_2:bool)) { .flag_semantics_$PSTATE_C = "(C (Diff ($R3, $R4)))"; .flag_semantics_$PSTATE_N = "(N (Diff ($R3, $R4)))"; .flag_semantics_$PSTATE_V = "(V (Diff ($R3, $R4)))";
+             .flag_semantics_$PSTATE_Z = "(Z (Diff ($R3, $R4)))" };
+         $PSTATE_V:bv1 := 0x0:bv1;
+         $PSTATE_C:bv1 := 0x0:bv1;
+         $PSTATE_Z:bv1 := 0x1:bv1;
+         $PSTATE_N:bv1 := 0x0:bv1;
+         goto (%block_4);
+       ];
+       block %block_4 [ (var BranchTaken:bool := false, $PC:bv64 := 0x8:bv64); goto (%block_5); ];
+       block %block_5 { .asm = "csel x0, x1, x2, cs" } [ var local_3:bv64 := 0x0:bv64; var local_3:bv64 := $R1; var local_4:bv64 := 0x0:bv64; var local_4:bv64 := $R2; goto (%block_7,%block_6); ];
+       block %block_6 [
+         assume eq($PSTATE_C, 0x1:bv1) { .flag_semantics_$PSTATE_C = "(Ite (EQ {z = (Diff ($R3, $R4))}, (Const Never), (C (Diff ($R5, $R6)))))";
+             .flag_semantics_$PSTATE_N = "(Ite (EQ {z = (Diff ($R3, $R4))}, (Const Never), (N (Diff ($R5, $R6)))))";
+             .flag_semantics_$PSTATE_V = "(Ite (EQ {z = (Diff ($R3, $R4))}, (Const Never), (V (Diff ($R5, $R6)))))";
+             .flag_semantics_$PSTATE_Z = "(Ite (EQ {z = (Diff ($R3, $R4))}, (Const Always), (Z (Diff ($R5, $R6)))))" };
+         $R0:bv64 := local_3:bv64;
+         goto (%block_8);
+       ];
+       block %block_7 [
+         assume boolnot(eq($PSTATE_C, 0x1:bv1)) { .flag_semantics_$PSTATE_C = "(Ite (EQ {z = (Diff ($R3, $R4))}, (Const Never), (C (Diff ($R5, $R6)))))";
+             .flag_semantics_$PSTATE_N = "(Ite (EQ {z = (Diff ($R3, $R4))}, (Const Never), (N (Diff ($R5, $R6)))))";
+             .flag_semantics_$PSTATE_V = "(Ite (EQ {z = (Diff ($R3, $R4))}, (Const Never), (V (Diff ($R5, $R6)))))";
+             .flag_semantics_$PSTATE_Z = "(Ite (EQ {z = (Diff ($R3, $R4))}, (Const Always), (Z (Diff ($R5, $R6)))))" };
+         $R0:bv64 := local_4:bv64;
+         goto (%block_8);
+       ];
+       block %block_8 [ (var BranchTaken:bool := false, $PC:bv64 := 0xc:bv64); goto (%ret_1); ];
+       block %ret_1 [ return; ]
+    ];
+    prog entry @main;
+    |}]
